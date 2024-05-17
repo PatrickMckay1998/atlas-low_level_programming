7dd73de07e935418c0ad04d7e2b1db29a3985098
root <patrick.mckay@atlasschool.com>
2024-05-17 03:09:54 +0000

    thur

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
 int main(void)
+int puts (const char *Programming is like building a multilingual puzzle);
+
 {
-	puts ("Programming is like building a multilingual puzzle");
+	puts ("\"Programming is like building a multilingual puzzle,");
+
 	return (0);
 }
