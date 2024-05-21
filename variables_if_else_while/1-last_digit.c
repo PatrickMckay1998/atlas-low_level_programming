0c220e3eb489cf69e71bd114cbef32bb163f3908
root <patrick.mckay@atlasschool.com>
2024-05-21 04:58:31 +0000

    mon

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -14,7 +14,7 @@ int main(void)
 		int ld = n % 10;
 		if (n < 6 && n != 0)
 		{
-			printf("Last digit of %d is %d and is less than 6 but not 0\n", n, ld);
+			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
 		}
 		if (n > 5) 
 		{
