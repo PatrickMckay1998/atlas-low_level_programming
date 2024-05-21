1922b41265d5f59a78e08153cd8a6dc2b528f060
root <patrick.mckay@atlasschool.com>
2024-05-21 04:55:19 +0000

    mon

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -18,11 +18,11 @@ int main(void)
 		}
 		if (n > 5) 
 		{
-			printf("Last digit of %d is %d and is greater than 5\n", n, ld);
+			printf("\nLast digit of %d is %d and is greater than 5\n", n, ld);
 		}
 		if (n == 0)
 		{
-			printf("Last digit of %d is %d and is 0\n", n, ld);
+			printf("\nLast digit of %d is %d and is 0\n", n, ld);
 		}
 	}
 	return (0);
