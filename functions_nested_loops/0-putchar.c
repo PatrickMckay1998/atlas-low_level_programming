82c9c998ab70c79dd3084b81f8128cd69b15812a
root <patrick.mckay@atlasschool.com>
2024-05-26 15:41:10 +0000

    sun

--- functions_nested_loops/0-putchar.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-	fwrite (_putchar);
-	return(0);
-}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef main_h
 #define main_h
 
-int _putchar
+int _putchar(char c);
+void print_aplhabet(void);
 
 #endif
