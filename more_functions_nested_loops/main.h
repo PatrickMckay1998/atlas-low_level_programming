5e4fb8071820c159661d9855eae0aa4d633b0d7d
root <patrick.mckay@atlasschool.com>
2024-05-26 15:24:53 +0000

    sun

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -3,16 +3,7 @@
 
 int _isupper(int c);
 {
-	char c;	
-	scanf("%c" , c)
-	if (isupper(c))
-	{
-		printf("1");
-	}
-	else if (islower(c))
-	{
-		printf("0");
-	}
-	return(0);
+	if (c >= 'A' && c <= 'Z')
+		return (1);
 }
 #endif
