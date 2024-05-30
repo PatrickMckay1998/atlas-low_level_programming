aece70e0d81f2d9ef3c0cbb18cc203ea92b2849d
root <patrick.mckay@atlasschool.com>
2024-05-30 16:29:02 +0000

    thur

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "stdio.h"
+#include "main.h"
+
+/**
+ * main - prints the aplphabet in lowercase
+ *
+ * Return: Always 0
+ *
+ * 
+ */
+
+int main(void)
+{
+	char al;
+	for(al = 'a' ; al <= 'z'; al++) 
+	{
+	_putchar(al);
+	}
+	_putchar('\n');
+	return (0);
+}
