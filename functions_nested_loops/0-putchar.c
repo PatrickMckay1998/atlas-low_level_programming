ac67fcbf6c7cd3a708ffe328b26a2ec81162a6d3
root <patrick.mckay@atlasschool.com>
2024-05-30 15:59:54 +0000

    thur

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,9 +1,19 @@
-#include <stdio.h>
 #include "main.h"
 
+/**
+ * main - prints out "_putchar" without using the stdio.h library
+ *
+ * Return - Always zero
+ *
+ * 
+ */
+
 int main(void)
-{	
-	c = _putchar
-	putchar(int _putchar(c));
+{
+	char *str = "_putchar";
+	while (*str)
+	{
+		_putchar(*str)	
+	}
 	return(0);
 }
