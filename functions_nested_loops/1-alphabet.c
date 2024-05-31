133214a68ed73e4be2744b33f0f56f600a448bc2
root <patrick.mckay@atlasschool.com>
2024-05-31 21:20:11 +0000

    fri

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -6,15 +6,14 @@
  * Return: Always 0
  *
  * 
- */
-int _putchar(char c);
+*/
 
 void print_alphabet(void)
 {
 	int c;
 	for(c = 'a' ; c <= 'z'; c++) 
 	{
-	_putchar('c');
+	_putchar(c);
 	}
 	_putchar('\n');
 }
