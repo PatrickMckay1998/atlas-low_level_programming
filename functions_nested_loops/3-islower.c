db345c96476585358bdc96989c7028587a5cb7d4
root <patrick.mckay@atlasschool.com>
2024-06-02 20:38:32 +0000

    sun

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/*
+ *  checks for lowercase characters
+ *
+ *
+ *
+ */
+
+int main(void)
+{	
+	int c;
+	if(_islower(c) != 0)
+	{
+		_putchar(1);
+	}	
+	else 
+	{
+		_putchar(0);
+	}
+	return(0);
+}
