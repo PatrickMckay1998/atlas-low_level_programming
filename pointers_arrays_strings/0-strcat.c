cead99436df305f7754d6762d3afeac58e9c1649
root <patrick.mckay@atlasschool.com>
2024-06-03 18:51:32 +0000

    mon

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -6,14 +6,9 @@ char *_strcat(char *dest, char *src)
 	{
         	++dest;
     	}	
-
-    // Copy characters of the second string to the end of
-    // the first string
     	while (*src) 
     	{
         	*dest++ = *src++;
     	}
-
-    // Add the null-terminating character
     *dest = '\0';
 }
