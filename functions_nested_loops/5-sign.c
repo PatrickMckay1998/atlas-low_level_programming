cfe3e55ad7bc9ad35e3bc642821d938722693e65
root <patrick.mckay@atlasschool.com>
2024-06-05 15:01:20 +0000

    wed

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -11,12 +11,12 @@
         _putchar('+');
         return(1);
     }
-    else if(n <= 0)
+    if(n <= 0)
     {
         _putchar('-');
         return('-1');
     }
-    else (n == 0)
+    if (n == 0)
     {
 	_putchar('0');
 	return(0);
