c3171d952bd77813553197b1ef5fd95c9acad1fa
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-06 17:38:40 -0500

    Putting the sum of I and J into quotes

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -8,11 +8,11 @@
 
 void times_table(void)
 {   
-    char m;
+    int m;
     int i, j;
-        for (i = 0; i <= '9'; i++)
+        for (i = 0; i <= 9; i++)
         {
-            for (j = 0; j <= '9'; j++)
+            for (j = 0; j <= 9; j++)
             {
                 m = (j * i);
                 _putchar('m');
