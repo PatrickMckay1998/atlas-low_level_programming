ae5f60614b698801193b829a3af04a859fdc8f08
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-06 18:30:18 -0500

    Change main.h and attempt to add integers

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/*
+* add - adds two integers and returns results
+*
+* Return - Always 0
+*
+*/
+
+int add(int, int)
+{
+    int mia, pat;
+    reslut = mia + pat;
+    return(result);
+}
