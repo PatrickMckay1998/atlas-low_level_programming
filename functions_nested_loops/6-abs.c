b6c3b7155a245e61aa4651e548d912ea7472ba71
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-06 11:59:44 -0500

    thur

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,10 @@
+#incude "main.h"
+
+int _abs(int)
+{
+    if(number >= 0)
+    {
+        return(number)
+    }
+    return (-numbers)
+}
--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -2,18 +2,6 @@
 
 int print_last_digit(int)
 {
-    int n;
-    int i;
-    if(n >= 1)
-    {
-        i = n % 10;
-        return(i);
-    }
-    else if (n <= -1)
-    {
-        i = n % 10;
-        return(i);
-    }
-    else 
-    return(0)
+    int lastdigit;
+    
 }
