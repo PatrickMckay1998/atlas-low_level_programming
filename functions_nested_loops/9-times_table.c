8bf3effc639424cb2416871305b32a0b41ebb541
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-06 17:34:09 -0500

    Trying to initialize 'm' as char

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -8,12 +8,13 @@
 
 void times_table(void)
 {   
+    char m;
     int i, j;
         for (i = 0; i <= 9; i++)
         {
             for (j = 0; j <= 9; j++)
             {
-                char m = (j * i);
+                m = (j * i);
                 _putchar(m);
                 _putchar(',');
                 _putchar(' ');
