a231ea1ea3b2cf9d7cfc6bbf2b28426fb37666aa
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-06 17:39:15 -0500

    Small changes

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -14,7 +14,7 @@ void times_table(void)
         {
             for (j = 0; j <= 9; j++)
             {
-                m = (j * i);
+                'm' = (j * i);
                 _putchar('m');
                 _putchar(',');
                 _putchar(' ');
