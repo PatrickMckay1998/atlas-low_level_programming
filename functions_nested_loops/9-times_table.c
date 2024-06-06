94863388c8bdb35302af3a859d98a02d9a0da65a
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-06 17:37:45 -0500

    Putting quotes around integers

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -10,12 +10,12 @@ void times_table(void)
 {   
     char m;
     int i, j;
-        for (i = 0; i <= 9; i++)
+        for (i = 0; i <= '9'; i++)
         {
-            for (j = 0; j <= 9; j++)
+            for (j = 0; j <= '9'; j++)
             {
                 m = (j * i);
-                _putchar(m);
+                _putchar('m');
                 _putchar(',');
                 _putchar(' ');
             }
