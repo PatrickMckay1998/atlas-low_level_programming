25afbf55419a20891ef4a592d8945e2400eee831
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-06 17:41:00 -0500

    changing m to int

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -13,7 +13,7 @@ void times_table(void)
         {
             for (j = 0; j <= 9; j++)
             {
-                m = (j * i);
+                int m = (j * i);
                 _putchar('m');
                 _putchar(',');
                 _putchar(' ');
