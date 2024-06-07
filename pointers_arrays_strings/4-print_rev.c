7df29d58ae740a1ad5f58d8bece4ece525e9b3ac
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-06 23:56:18 -0500

    Update main.h and new file

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+void print_rev(char *s)
+{
+    int length = 0;
+    while (s[length] != '\0')
+    {
+        length++;
+    }
+    for (int i = length - 1; i >= 0; i--)
+    {
+        _putchar(s[i]);
+    }
+    _putchar('\n');  
+}
