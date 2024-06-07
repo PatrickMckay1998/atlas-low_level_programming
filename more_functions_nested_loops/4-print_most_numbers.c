e450061e5d20bac2552c9a292b1c5f5f712eba51
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-06 21:38:46 -0500

    Changed m from int to char

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -2,12 +2,12 @@
 
 void print_most_numbers(void)
 {
-    int m;
+    char m;
     for (m = 0; m <= 9; m++)
     {
         if (m != 2 && m != 4)
         {
-            _putchar('m');
+            _putchar(m);
         }
     }    
     _putchar('\n');
