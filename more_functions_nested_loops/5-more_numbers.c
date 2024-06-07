b2b431efa5a520d5bb5453d1fe741be65930743a
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-06 22:21:02 -0500

    Fixe dequation

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -3,12 +3,12 @@
 void more_numbers(void)
 {
     int m, p;
-    for (m = '0'; m <= '10'; m++)
+    for (m = 0; m <= 10; m++)
     {
-        for (p = '0'; p <= '14'; p++)
+        for (p = 0; p <= 14; p++)
         {
             if (p >= 10)
-            _putchar( '0' = p / 10);
+            _putchar( '0' + p / 10);
             _putchar ('0' + p % 10);
         }
     _putchar('\n');
