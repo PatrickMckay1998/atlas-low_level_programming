35a6f0fac1aa68019de557d736673735bb32faf2
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-06 19:03:44 -0500

    First commit

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/*
+* print to 98 - prints all natural numbers from input to 98
+*
+* Return - Always 0
+*
+*/
+
+void print_to_98(int n)
+{
+    if (n <= 98)
+    {
+        for (; n <= 98; n++)
+        {
+            _putchar('n');
+            if (n != 98)
+            {
+                _putchar(',');
+                _putchar(' ');
+            }
+        }
+    }
+}
+    else 
+{
+    for (; n >= 98, n--)
+    {
+        _putchar('n');
+    
+        if (n != 98)
+        {_putchar(',');
+        _putchar(' ');
+        }
+    }
+    _putchar('\n');
+}
