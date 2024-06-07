ee2f115d5c8b8abf152d6e6f177ffd0b164725d1
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-06 23:13:42 -0500

    Fixed errors, added spaces

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -8,18 +8,22 @@ int main(void)
         if (m % 3)
         {
             printf("Fizz");
+            printf(" ");
         }
         if (m % 5)
         {
             printf("Buzz");
+            printf(" ");
         }
         if (m % 3 && m % 5)
         {
-            printf("FizzBuzz")
+            printf("FizzBuzz");
+            printf(" ");
         }
         else 
         {
             printf("%d", m);
+            printf(" ");
         }
     }
     return (0);
