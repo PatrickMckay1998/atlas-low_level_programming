2bd89b714650fb99cbf906f78c7143f8b92c20be
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-06 22:45:22 -0500

    New file and updated main.h

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "main.c"
+
+void print_diagonal(int n)
+{
+    int m, p;
+    if (n <= 0)
+    {
+        _putchar('\n');
+    }
+    for (m = 0; m < n; m++)
+    {
+        for (p = 0; p < m; p++)
+        {
+            _putchar(' ');
+        }
+        _putchar('\\');
+        _putchar('\n');
+    }
+
+}
