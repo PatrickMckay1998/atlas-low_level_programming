c619202c155410c5641df76ce41c56dd9e5a1054
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-06 23:15:58 -0500

    Changed arithmetic

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -5,17 +5,17 @@ int main(void)
     int m;
     for(m = 1; m <= 100; m++)
     {
-        if (m % 3)
+        if (m / 3)
         {
             printf("Fizz");
             printf(" ");
         }
-        if (m % 5)
+        if (m / 5)
         {
             printf("Buzz");
             printf(" ");
         }
-        if (m % 3 && m % 5)
+        if (m / 3 && m / 5)
         {
             printf("FizzBuzz");
             printf(" ");
