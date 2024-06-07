28fe336bf98200e8c4bdd8c29a10d5edc35b97fe
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-06 21:02:15 -0500

    Trying different if loop

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -10,16 +10,12 @@
 
 int _isdigit(int c)
 {
-    if (c >= 'a' && c <= 'z')
+    if (c >= '0' && c <= '9')
     {
-        return (0);
-    }
-    else if (c >= 'A' && c <= 'Z')
-    {
-        return (0);
+        return (1);
     }
     else
     {
-        return(1);
+        return (0);
     }
 }
