b6dade357f864fdad56bedd8063fdeee42033333
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-06 19:12:50 -0500

    try

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -13,7 +13,7 @@ void print_to_98(int n)
     {
         for (; n <= 98; n++)
         {
-            _putchar('0' + n);
+            _putchar(n);
             if (n != 98)
             {
                 _putchar(',');
@@ -26,7 +26,7 @@ void print_to_98(int n)
 {
     for (; n >= 98; n--)
     {
-        _putchar('0' + n);
+        _putchar(n);
     
         if (n != 98)
         {
