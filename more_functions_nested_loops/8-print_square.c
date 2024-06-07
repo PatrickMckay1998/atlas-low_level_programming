885d985fcd9d5205a495b3db49f19e8d0e65b047
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-06 22:53:47 -0500

    New file and updated main.h

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+void print_square(int size)
+{
+    int m;
+    if (size <= 0)
+    {
+        _putchar('\n');
+    }
+    for (m = 0; m < size; m++)
+    {
+        _putchar('#');
+        _putchar('\n'):
+    }
+}
