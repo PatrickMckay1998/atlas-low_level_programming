564b05d7b54f539a7e05bab9adc1da53710a9f04
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-06 19:17:45 -0500

    Add extra if statement for 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,11 +9,15 @@
 
 void print_to_98(int n)
 {
+    if (n == 98)
+    {
+        _putchar('n');
+    }
     if (n <= 98)
     {
         for (; n <= 98; n++)
         {
-            _putchar(n);
+            _putchar('n');
             if (n != 98)
             {
                 _putchar(',');
@@ -26,7 +30,7 @@ void print_to_98(int n)
 {
     for (; n >= 98; n--)
     {
-        _putchar(n);
+        _putchar('n');
     
         if (n != 98)
         {
