a562ebdf9e651d333580b5052446abe045684eb3
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-09 18:13:51 -0500

    Updated main, created file

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+int _sqrt_helper(int n, int i)
+        return -1;
+    if (i * i == n)
+        return i;
+    return _sqrt_helper(n, i + 1);
+}
+
+int _sqrt_recursion(int n)
+{
+    if (n < 0)  // Natural square root does not exist for negative numbers
+        return -1;
+    return _sqrt_helper(n, 0);  // Start the helper function with 0 as the initial divisor
+}
