de1d9ef9c63334c493764beee05a94e3d5d12538
root <patrick.mckay@atlasschool.com>
2024-06-10 15:46:37 +0000

    mon

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
-char *string_toupper(char *)
+char *string_toupper(char m)
 {	
-	int i;
-	char m;
+	int i = 0;
 	while (m != '\0')
 	{
 		m[i] = m[i] + 32;
+		i++;
 	}
 	return(m);
 	
