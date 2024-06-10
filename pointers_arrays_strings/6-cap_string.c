dfbe0c45c9feb85d5684f7b8a9c62e357481ff5d
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-10 18:13:51 -0500

    Update main and create file

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+char *cap_string(char *str)
+{
+
+    char *separators = " \t\n,;.!?\"(){}"
+    char *ptr = str
+    int capnxt = 1;
+
+    while (*ptr != '\0')
+    {
+        if (capnxt && *ptr >= 'a' && *ptr <= 'z')
+        {
+            *ptr -= 32;
+            capnxt = 0;
+        }   
+        else if (separators != NULL && *ptr != NULL)
+        {
+            capnxt = 1;
+        }
+        else 
+        {
+            capnxt = 0;
+        }
+        ptr++;
+    }
+    return (str);
+}
