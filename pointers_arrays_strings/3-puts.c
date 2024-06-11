1a3c22261c3eda5aa5bdf2e3818814a03632b6a5
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-11 17:42:01 -0500

    Added description

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,6 +1,14 @@
 #include "main.h"
 
-
+/**
+ * _puts - Prints a string followed by a new line
+ * @str: The string to be printed
+ *
+ * Description: This function takes a string and prints it to stdout,
+ * followed by a new line.
+ *
+ * Return: void
+ */
 
 void _puts(char *str)
 {
