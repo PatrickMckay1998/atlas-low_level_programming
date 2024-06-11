66d1818f79634e11d6cec344b0a79c21ae4a181a
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-11 17:23:21 -0500

    Stupid whitespace

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -4,7 +4,7 @@
  * puts_half - Prints the second half of a string
  * @str: The string to be processed
  *
- * Description: This function takes a string and prints the second half of 
+ * Description: This function takes a string and prints the second half of
  * the string, followed by a new line. If the number of characters is odd,
  * it prints the last n characters where n = (length_of_the_string - 1) / 2
  *
