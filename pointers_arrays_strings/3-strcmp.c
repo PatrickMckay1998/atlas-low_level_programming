ff270b4bdcddab08d74ea5ccd5f00c1d433d851e
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-10 22:58:54 -0500

    Ye

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -14,19 +14,19 @@ while (s1[i] != '\0' && s2[i] != '\0')
 {
 if (s1[i] != s2[i])
 {
-return s1[i] - s2[i];
+return (s1[i] - s2[i]);
 }
 i++;
 }
 
 if (s1[i] == '\0' && s2[i] != '\0')
 {
- return -s2[i];
+ return (-s2[i]);
 }
 else if (s1[i] != '\0' && s2[i] == '\0')
 {
-return s1[i];
+return (s1[i]);
 }
 
-return 0;
+return (0);
 }
