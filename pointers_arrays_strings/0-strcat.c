ec7c0c3c8438fb0b540ffa6dd9f44b630df1be79
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-10 20:53:02 -0500

    Betty be chill af

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -8,21 +8,21 @@
 */
 char *_strcat(char *dest, char *src)
 {
-    char *dest_end = dest;
+char *dest_end = dest;
 
-    while (*dest_end != '\0')
-    {
-        dest_end++;
-    }
+while (*dest_end != '\0')
+{
+dest_end++;
+}
 
-    while (*src != '\0')
-    {
-        *dest_end = *src;
-        dest_end++;
-        src++;
-    }
+while (*src != '\0')
+{
+*dest_end = *src;
+dest_end++;
+src++;
+}
 
-    *dest_end = '\0';
+*dest_end = '\0';
 
-    return (dest);
+return (dest);
 }
