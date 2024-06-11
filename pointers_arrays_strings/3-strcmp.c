3f09afcad2f4b72a6f74b0ccce67fbef7de750ae
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-10 22:54:34 -0500

    I hope betty likes this

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,26 +1,31 @@
 #include "main.h"
 
+/*
+* _strcmp - compares two strings by iterating through the strings and comparing characters
+* @i: this is what checks every character to see if they are the same
+*/
+
 int _strcmp(char *s1, char *s2)
 {
-    int i = 0;
+int i = 0;
 
-    while (s1[i] != '\0' && s2[i] != '\0')
-    {
-        if (s1[i] != s2[i])
-        {
-            return s1[i] - s2[i];
-        }
-        i++;
-    }
+while (s1[i] != '\0' && s2[i] != '\0')
+{
+if (s1[i] != s2[i])
+{
+return s1[i] - s2[i];
+}
+i++;
+}
 
-    if (s1[i] == '\0' && s2[i] != '\0')
-    {
-        return -s2[i];
-    }
-    else if (s1[i] != '\0' && s2[i] == '\0')
-    {
-        return s1[i];
-    }
+if (s1[i] == '\0' && s2[i] != '\0')
+{
+ return -s2[i];
+}
+else if (s1[i] != '\0' && s2[i] == '\0')
+{
+return s1[i];
+}
 
-    return 0;
+return 0;
 }
