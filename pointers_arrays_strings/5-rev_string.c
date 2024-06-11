778f04a9b291ef296c8544fb3066872c90ccee7c
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-11 16:51:15 -0500

    Update betty style

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,18 +1,29 @@
 #include "main.h"
 
+/*
+* rev_string - reverses a string by replacing the beginning with the end
+*
+* @length: length of the string
+* @i: is the counter
+* @temp: becomes the placeholder for the end of the string to the beginning
+* Return - Always 0
+*
+*/
+
+
 void rev_string(char *s)
 {
-    int length = 0;
-    int i;
-    char temp;
-    while (s[length] != '\0')
-    {
-        length++;
-    }
-    for (i = 0; i < length / 2; i++)
-    {
-        temp = s[i];
-        s[i] = s[length -1 -i];
-        s[length -1 -i] = temp;
-    }
+int length = 0;
+int i;
+char temp;
+while (s[length] != '\0')
+{
+length++;
+}
+for (i = 0; i < length / 2; i++)
+{
+temp = s[i];
+s[i] = s[length -1 -i];
+s[length -1 -i] = temp;
+}
 }
