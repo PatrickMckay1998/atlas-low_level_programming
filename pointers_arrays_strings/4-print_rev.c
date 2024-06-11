c1d0d7f066380174c767ee9b851f3bd02a9f8652
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-11 17:35:52 -0500

    Added new line and fixewd the line endings

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -2,15 +2,15 @@
 
 void print_rev(char *s)
 {   
-    int i;
-    int length = 0;
-    while (s[length] != '\0')
-    {
-        length++;
-    }
-    for (i = length - 1; i >= 0; i--)
-    {
-        _putchar(s[i]);
-    }
-    _putchar('\n');  
+int i;
+int length = 0;
+while (s[length] != '\0')
+{
+length++;
+}
+for (i = length - 1; i >= 0; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');  
 }
