b83b87061a1ab0f5fffc297e0707a04ec3ea47af
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-10 20:58:38 -0500

    First try?

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,18 +1,23 @@
 #include "main.h"
+/*
+* _strncpy - copies a string
+* @dest - the destination string
+* @src - the source string
+**/
 
 char *_strncpy(char *dest, char *src, int n)
 {
-    int i;
+int i;
 
-    for (i = 0; i < n && src[i] != '\0'; i++)
-    {
-        dest[i] = src[i];
-    }
+for (i = 0; i < n && src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
 
-    for (; i < n; i++)
-    {
-        dest[i] = '\0';
-    }
+for (; i < n; i++)
+{
+dest[i] = '\0';
+}
 
-    return dest;
+return dest;
 }
