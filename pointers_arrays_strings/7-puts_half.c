8f9ea5c402ee35c251dd75a3eeb81d9212284277
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-11 17:20:21 -0500

    Fixed line endings

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -2,25 +2,25 @@
 
 void puts_half(char *str)
 {
-    int len = 0, i, n;
+int len = 0, i, n;
 
-    while (str[len] != '\0')
-    {
-        len++;
-    }
+while (str[len] != '\0')
+{
+len++;
+}
 
-    if (len % 2 == 0)
-    {
-        n = len / 2;
-    }
-    else
-    {
-        n = (len - 1) / 2 + 1;
-    }
+if (len % 2 == 0)
+{
+n = len / 2;
+}
+else
+{
+n = (len - 1) / 2 + 1;
+}
 
-    for (i = n; i < len; i++)
-    {
-        _putchar(str[i]);
-    }
-    _putchar('\n');
+for (i = n; i < len; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
 }
