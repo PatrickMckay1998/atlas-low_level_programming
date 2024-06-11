26cfd840c251fa5577687e9f93cbbbed71b7f5aa
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-11 17:46:09 -0500

    Added description

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,6 +1,14 @@
 #include "main.h"
 
-
+/**
+ * _strlen - Returns the length of a string
+ * @s: The string to measure
+ *
+ * Description: This function takes a string as input and returns
+ * the length of the string, excluding the terminating null byte (\0).
+ *
+ * Return: The length of the string
+ */
 
 int _strlen(char *s)
 {
