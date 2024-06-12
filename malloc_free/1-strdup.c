6171587c67daa01652332981c3c16e5e021d8b2d
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-12 16:10:04 -0500

    Fixed errors

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -3,7 +3,7 @@
 
 char *_strdup(char *str)
 {
-    char str_two[] = malloc(sizeof(/*OG str?*/) * 4)
+    char str_two[] = malloc(sizeof(str) * 4)
     int i = 0;
     
     if (str = NULL)
@@ -14,6 +14,6 @@ char *_strdup(char *str)
     for(i = 0, i < str; i++)
     { /* Here is where the counter goes up
         I is going up but where is the counter going*/
-        return (str_two[i]);
+        return (*str_two[i]);
     }
 }
