1a48492af80dc0fdf65337a69a432c8c572259a4
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-12 16:05:37 -0500

    Update main  and created file, attempt program

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+char *_strdup(char *str)
+{
+    str_two[] = malloc(sizeof(/*OG str?*/) * 4)
+    int i = 0;
+    
+    if (str = NULL)
+    {
+        return (NULL);
+    }
+    
+    for(i = 0, i < str; i++)
+    { /* Here is where the counter goes up
+        I is going up but where is the counter going*/
+        return (str_two[i]);
+    }
+
+}
