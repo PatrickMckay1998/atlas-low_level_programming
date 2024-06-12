19b8558eae053b6639af248c694187e83e36b4c4
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-11 23:21:39 -0500

    REdid the description

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,10 +1,18 @@
 #include "main.h"
 
-/*
-* _strcmp - compares two strings by iterating through the strings
-*           and comparing characters
-* @i: this is what checks every character to see if they are the same
-*/
+/**
+ * _strcmp - Compares two strings
+ * @s1: The first string to be compared
+ * @s2: The second string to be compared
+ *
+ * Description: This function compares two strings character by character.
+ * It returns an integer less than, equal to, or greater than zero if the first
+ * string is found, respectively, to be less than, to match, or be greater than
+ * the second string.
+ *
+ * Return: An integer less than, equal to, or greater than zero if s1 is found,
+ * respectively, to be less than, to match, or be greater than s2.
+ */
 
 int _strcmp(char *s1, char *s2)
 {
