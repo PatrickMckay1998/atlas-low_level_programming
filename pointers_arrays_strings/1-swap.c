d044602846fb8596bf6e5bf45aa40113f005ca25
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-11 23:10:43 -0500

    Added description

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,5 +1,16 @@
 #include "main.h"
 
+/**
+ * swap_int - Swaps the values of two integers
+ * @a: The first integer to be swapped
+ * @b: The second integer to be swapped
+ *
+ * Description: This function swaps the values of two integers using a temporary
+ * variable to hold one of the values during the swap process.
+ *
+ * Return: void
+ */
+
 void swap_int(int *a, int *b)
 {
 int temp = *a;
