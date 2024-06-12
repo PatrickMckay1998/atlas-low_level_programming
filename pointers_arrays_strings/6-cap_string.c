16b4fc68f646c90931cd4d00e43973a264600c8a
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-11 23:41:26 -0500

    fixed

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -3,39 +3,39 @@
 char *cap_string(char *str)
 {
 
-    char sep[] = { ' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}', '\0' };
-    char *ptr = str;
-    int capnxt = 1;
+char sep[] = { ' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}', '\0' };
+char *ptr = str;
+int capnxt = 1;
 
-    while (*ptr != '\0')
-    {
-        if (capnxt && *ptr >= 'a' && *ptr <= 'z')
-        {
-            *ptr -= 32;
-            capnxt = 0;
-        }
-        else 
-        {
-            int is_sep = 0;
-            int i;
-            for (i = 0; sep[i] != '\0'; i++)
-            {
-                if (*ptr == sep[i])
-                {
-                    is_sep = 1;
-                    break;
-                }
-            }
-            if (is_sep)
-            {
-                capnxt =1;
-            }
-            else
-            {
-                capnxt = 0;
-            }
-        }
-        ptr++;
-    }
-    return (str);
+while (*ptr != '\0')
+{
+if (capnxt && *ptr >= 'a' && *ptr <= 'z')
+{
+*ptr -= 32;
+capnxt = 0;
+}
+else 
+{
+int is_sep = 0;
+int i;
+for (i = 0; sep[i] != '\0'; i++)
+{
+if (*ptr == sep[i])
+{
+is_sep = 1;
+break;
+}
+}
+if (is_sep)
+{
+ capnxt =1;
+}
+else
+{
+capnxt = 0;
+}
+}
+ptr++;
+}
+return (str);
 }
