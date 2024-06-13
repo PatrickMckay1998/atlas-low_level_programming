62f801eb745e6c60a3442dbb3b1e929f268f8ec9
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-13 12:03:01 -0500

    UPdate main, create file, fix previous issue

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -6,7 +6,7 @@ char *create_array(unsigned int size, char c)
 {   
     unsigned int i = 0;
     char *str_array;
-    str_array = malloc(sizeof(char) * size + 1);
+    str_array = malloc(size);
 
     if (str_array == NULL)
     {
---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+char *str_concat(char *s1, char *s2)
+{
+    /* concatenate two strings
+    - we know we have two strings
+    - we know we are going to be using a for loop and a while loop
+    - */
+    char *end_of_line;
+    unsigned int i;
+    
+     if ()
+}
