30d4230d1edb1746a026e16fe89041b17e6fc95a
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-13 22:46:57 -0500

    Updated main, created new file and try it out

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -6,12 +6,31 @@ char *str_concat(char *s1, char *s2)
     - we know we have two strings
     - we know we are going to be using a for loop and a while loop
     - */
-    char *end_of_line;
-    unsigned int i;
 
-     if (s1 == NULL || s2 == NULL)
-     {
+    char new_str;
+    char dest_end = s1;
+    int i;
+     if (s1 == NULL || s2 == NULL) /* Return NULL if either string is NULL*/
+    {
         return (NULL);
-     }
-     
+    }
+
+    dest_end = malloc(sizeof(s1) && (s2));
+    i = 0;
+    while (dest_end != '\0') /* loops through dest end until '\0' and creates ptr to end of dest end*/
+    {
+        dest_end++;
+    }
+
+    while (s2 != '\0') /* this loop copies each character from string 2
+        to the position pointed ot by the dest end. After each copy, both dest end and string 2
+        are incremented to point to the next character in their respective strings*/
+    {
+        dest_end = s2; 
+        dest_end++;
+        s2++;
+    }
+    
+    dest_end = '\0'; /* Add a null byte to the end*/
+    return (s1);
 }
--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -10,19 +10,19 @@ char *_strcat(char *dest, char *src)
 {
 char *dest_end = dest;
 
-while (*dest_end != '\0')
+while (*dest_end != '\0') /* while the destination end does not equal NULL, it also starts at zero in the array*/
 {
-dest_end++;
+dest_end++; /* destination end increment by one in the array*/
 }
 
-while (*src != '\0')
+while (*src != '\0') /* while the source does not equal null*/
 {
-*dest_end = *src;
-dest_end++;
-src++;
+*dest_end = *src; /* destination end equals source*/
+dest_end++; /* destination end increment to two*/
+src++; /* source increments to one and goes back up to */
 }
 
-*dest_end = '\0';
+*dest_end = '\0'; /* this is adding the null byte at the end, I think. */
 
 return (dest);
 }
