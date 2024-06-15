8a8477856293265268d1d9a3ffeeab5af997fe1e
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-14 20:26:12 -0500

    Added comments

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -2,36 +2,40 @@
 #include <stdlib.h>
 
 char *str_concat(char *s1, char *s2)
-{
-    while (s1[len1] != '\0')
+{   
+    int len1, len2;
+    char *new_str;
+    char *dest, *result;
+
+    while (s1[len1] != '\0') /*Calculate length of s1*/
     {
         len1++;
     }
 
-    while (s2[len2] != '\0')
+    while (s2[len2] != '\0') /*Calculate length of s2*/
     {
         len2++;
     }
 
-    char *new_str = (char *)malloc(len1 + len2 +1);
+    char *new_str = (char *)malloc(len1 + len2 +1); /* Allocate memory for new string which is the amount of s1 plus s2 */
 
-    if (new_str == NULL)
+    if (new_str == NULL) /* NULL statement out of the way */
     {
         return (NULL);
     }
 
-    char *dest = *result;
+    char *dest = *result; /* Setting the dest ptr to equal the result ptr*/
     
-    while (*s1 != '\0')
+    while (*s1 != '\0') /* Copies s1 to dest ptr */
     {
         *dest++ = *s1++;
     }
 
-    while (*s2 != '\0')
+    while (*s2 != '\0') /* Copies s2 to dest ptr*/
     {
         *dest++ = *s2++;
     }
 
-    *dest = '\0';
-    return (result);
+    *dest = '\0'; /* Adds null byte to end of dest ptr*/
+    return (result); /* returns result ptr with allocated memory*/
 }
