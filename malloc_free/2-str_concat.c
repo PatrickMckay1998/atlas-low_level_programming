d4cb338f14e89662f2a6942009a648b12e6efe46
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-14 20:29:13 -0500

    REmoved ptr's because why not

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -19,23 +19,23 @@ char *str_concat(char *s1, char *s2)
 
     result = (char *)malloc(len1 + len2 +1); /* Allocate memory for new string which is the amount of s1 plus s2 */
 
-    if (new_str == NULL) /* NULL statement out of the way */
+    if (resilt == NULL) /* NULL statement out of the way */
     {
         return (NULL);
     }
 
-    char *dest = *result; /* Setting the dest ptr to equal the result ptr*/
+    char dest = result; /* Setting the dest ptr to equal the result ptr*/
     
     while (*s1 != '\0') /* Copies s1 to dest ptr */
     {
-        *dest++ = *s1++;
+        dest++ = s1++;
     }
 
     while (*s2 != '\0') /* Copies s2 to dest ptr*/
     {
-        *dest++ = *s2++;
+        dest++ = s2++;
     }
 
-    *dest = '\0'; /* Adds null byte to end of dest ptr*/
+    dest = '\0'; /* Adds null byte to end of dest ptr*/
     return (result); /* returns result ptr with allocated memory*/
 }
