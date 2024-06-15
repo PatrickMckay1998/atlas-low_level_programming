7d2fb7f6a09c73f9ee6a0dc552ab3f7ec86cf0e5
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-14 21:14:08 -0500

    Should work just fine

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -24,16 +24,16 @@ char *str_concat(char *s1, char *s2)
         return (NULL);
     }
 
-    char dest = *result; /* Setting the dest ptr to equal the result ptr*/
+    *dest = *result; /* Setting the dest ptr to equal the result ptr*/
     
     while (*s1 != '\0') /* Copies s1 to dest ptr */
     {
-        dest++ = s1++;
+        *dest++ = *s1++;
     }
 
     while (*s2 != '\0') /* Copies s2 to dest ptr*/
     {
-        dest++ = s2++;
+        *dest++ = *s2++;
     }
 
     dest = '\0'; /* Adds null byte to end of dest ptr*/
