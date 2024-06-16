bf013fe39b76191a56f6b8ce48985d18d57897a3
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-16 10:43:34 -0500

    Saving progress, created new directory, updated readme, updated main

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,7 +1,21 @@
 #include "main.h"
 #include <stdlib.h>
 
-int **aloc_grid(int width, int height)
+int **alloc_grid(int width, int height)
 {
+    int width = 0;
+    int height = 0;
+    int 
+
+    if (width <= 0 || height <= 0)
+    {
+        return (NULL); /* If width or height are zero or less then return NULL */
+    }
+
+    while (m < width) /*Loop through the width and set it to 0*/
+    {   m = width;
+        width++;
+        m;
+    }
     
 }
