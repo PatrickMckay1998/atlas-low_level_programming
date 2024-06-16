11ad3bca6f8739aab2ee61b251b73d567c80cf99
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-16 12:32:48 -0500

    Attempt one

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include <stdlib.c>
+#include "main.h"
+
+/* Returns a pointer to the allocated memory
+    if malloc fails then the function should cause a normal process termination with a status value of 98
+*/
+
+void *malloc_checked(unsigned int b)
+{
+    unsigned int *return_ptr;
+    unsigned int diff_b = b;
+
+    return_ptr = (unsigned int *)malloc(sizeof(diff_b));
+
+    return (return_ptr);
+}
