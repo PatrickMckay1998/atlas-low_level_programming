37ee2929cf2d153909c025a043ef65cfe22c7174
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-17 12:41:39 -0500

    Maybe

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -3,12 +3,12 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {   
-    void *arr_1 = nmemb;
+    *arr_1 = nmemb;
     int i;
 
     arr_1 = malloc(size * nmemb);
 
-    if (nmemb == 0|| size == 0 || str_1 == 0)
+    if (nmemb == 0|| size == 0 || arr_1 == 0)
     {
         return (NULL);
     }
