c1ba996f7818cf28e75f3a7a718065b605660ad4
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-17 14:02:54 -0500

    Trying this

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -1,2 +1,26 @@
 #include "main.h"
 #include <stdlib.h>
+
+int *arraY_range(int min, int max)
+{   
+    int arr_1;
+    
+    arr_1 = malloc(max - min + 1);
+
+    if(min > max)
+    {
+        return (NULL);
+    }
+    
+    m = 0;
+    i = 0;
+
+    while (i >= min && i <= max)
+    {
+        j[m] = i;
+    }
+
+
+
+
+}
