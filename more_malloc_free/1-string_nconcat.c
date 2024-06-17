eea477f89249d262d120d0a92d36e15d502ed16b
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-16 23:09:01 -0500

    fix

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -4,7 +4,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {   
     char *str_1 = s1;
-    char m;
+    int m;
 
     if (s1 == NULL || s2 == NULL)
     {
@@ -18,7 +18,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
     str_1 = malloc(sizeof(s1) + sizeof(s2) + 1); /* Allocating memory */
 
-    while('m' < n && s2[m] != '\0') /* While m is less than int n and the m position in string two does not equal the null*/
+    m = 0;
+
+    while(m < n && s2[m] != '\0') /* While m is less than int n and the m position in string two does not equal the null*/
     {
         *str_1 = s2[m];
         str_1++;
