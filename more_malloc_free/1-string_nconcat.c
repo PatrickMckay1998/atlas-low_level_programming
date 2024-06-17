91a24ebecf3ce4487f61c37e5768b81f4359ce6c
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-16 23:31:27 -0500

    fix

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -16,7 +16,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
         str_1++;
     }
 
-    str_1 = malloc(str_1 + n + 1); /* Allocating memory */
+    str_1 = malloc(str_1 + 'n' + 1); /* Allocating memory */
 
     m = 0;
 
