93d05abf84a9bda787db4a657ef80530653bc4ea
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-16 22:55:14 -0500

    Update

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,32 @@
 #include "main.h"
 #include <stdlib.c>
 
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{   
+    char *str_1 = *s1;
+    int m;
 
+    if (/*NULL condition goes here*/)
+    {
+        return (NULL);
+    }
+
+    while (*str_1 <= '\0') /* Getting the length of s1*/
+    {
+        str_1++;
+    }
+
+    str_1 = malloc(sizeof(s1) * (s2)); /* Allocating memory */
+
+    m = 0;
+    while(m < n && s2[m] != '\0') /* While m is less than int n and the m position in string two does not equal the null*/
+    {
+        *str_1 = s2[m];
+        *str_1++;
+        m++;
+    }
+
+    return (s1);
+
+}
 
--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -14,12 +14,12 @@ char *_strncat(char *dest, char *src, int n)
 char *dest_end = dest;
 int i = 0;
 
-while (*dest_end != '\0')
+while (*dest_end <= '\0') /* getting the length of dest_end*/
 {
 dest_end++;
 }
 
-while (i < n && src[i] != '\0')
+while (i < n && src[i] != '\0') 
 {
 *dest_end = src[i];
 dest_end++;
