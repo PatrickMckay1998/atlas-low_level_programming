5810450b7c830b08609dc4d07e7f02947c81b1ab
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-17 22:24:22 -0500

    Pleasing the betty gods

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -2,23 +2,30 @@
 #include <stdlib.h>
 #include <string.h>
 
+/*
+* _strdup - duplicates a string
+* @dup: pointer for the duplicate string
+* @length: length of the string that duplicates
+*
+*/
+
 char *_strdup(char *str)
 {
-    char *dup;
-    unsigned int length;
-    dup = malloc(sizeof(str) * 25);
-    
-    if (str == NULL)
-    {
-        return (NULL);
-    }
-    
-    length = 0; 
-    
-    while(str[length])
-    {
-    dup[length] = str[length];
-    length++;
-    }
-    return (dup);
+char *dup;
+unsigned int length;
+dup = malloc(sizeof(str) * 25);
+
+if (str == NULL)
+{
+return (NULL);
+}
+
+length = 0;
+
+while(str[length])
+{
+dup[length] = str[length];
+length++;
+}
+return (dup);
 }
