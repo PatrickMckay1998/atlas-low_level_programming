ff66f5554ae4ed50744ceb5237bf239804c09bb8
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-17 22:16:24 -0500

    Betty

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -2,23 +2,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* 
+* *create_array - creates an array of chars, and initialized it
+* @i: used as a counter for the position in the array
+* @str_array: the new array 
+*/
+
 char *create_array(unsigned int size, char c)
 {   
-    unsigned int i = 0;
-    char *str_array;
-    str_array = malloc(size);
+unsigned int i = 0;
+char *str_array;
+str_array = malloc(size);
 
-    if (str_array == NULL)
-    {
-        return (NULL);
-    }
-    if (size == 0)
-    {
-        return (NULL);
-    }
-    for (i = 0; i < size; i ++)
-    {
-        str_array[i] = c;
-    }
-    return (str_array);
+if (str_array == NULL)
+{
+return (NULL);
+}
+if (size == 0)
+{
+return (NULL);
+}
+for (i = 0; i < size; i ++)
+{
+str_array[i] = c;
+}
+return (str_array);
 }
