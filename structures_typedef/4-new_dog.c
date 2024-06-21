977f3dea371b6e94ef6fac79b70835a633e6164d
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-21 11:05:09 -0500

    Updated main.h Added notes to new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -7,3 +7,7 @@ dog_t *new_dog(char *name, float age, char *owner)
     *name = malloc(char);
     *owner = malloc(char);
 }
+
+/* get the length of name and owner and allocate space for the name and owner
+create a copy of the name and owner into a new variable 
+*/
