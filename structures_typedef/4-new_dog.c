1b1a0d76e86f1762307c697d73008d3f1c97fdbb
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-20 23:46:28 -0500

    Save what I have and move on

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+    new_dog -> name = malloc(char);
+    new_dog -> owner = malloc(char);
+}
