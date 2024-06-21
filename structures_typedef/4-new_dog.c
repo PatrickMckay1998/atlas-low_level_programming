22508af0fd575e9e650811b0db2c958af2670e47
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-21 14:14:33 -0500

    Works on Python

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -4,10 +4,23 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    *name = malloc(char);
-    *owner = malloc(char);
+int len1, len2;
+char *cpy_name, *cpy_owner;
+  
+len1 = 0;
+  
+  while(name[len1])
+  {
+    cpy_name[len1] = name[len1];
+    len1++;
+  } /* obtains length of name and copies it to cpy_name */
+  
+  while (owner[len2])
+  {
+    cpy_owner[len2] = owner[len2];
+    len2++;
+  } /* obtaines length of owner and copies it to cpy_owner */
+  
+cpy_name = malloc(sizeof(name) + 1);
+cpy_owner = malloc(sizeof(owner) + 1);
 }
-
-/* get the length of name and owner and allocate space for the name and owner
-create a copy of the name and owner into a new variable 
-*/
