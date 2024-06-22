9d49957f9c9e7ccbfc44ff29c7bd86a99f4d0199
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-21 22:15:15 -0500

    Maybe it will work

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -4,9 +4,14 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+    int m;
+    size_t og_size = size; 
+
+    m = 0;
+
     while (array[m] != '\0')
     {
-        (*action)(array, size);
+        (*action)(*array, size);
         m++;
     }
 }
