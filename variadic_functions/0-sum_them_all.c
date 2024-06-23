a83668157288fef2d15212b2f2c25c3e30cdfd7d
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-23 15:24:45 -0500

    This might not be right but it is a start

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,3 +1,21 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+int sum_them_all(const unsigned int n, ...)
+{
+    int sum;
+    
+    va_list args;
+
+    va_start(args, n);
+    
+    if (n == 0)
+    {
+        return (0);
+    }
+
+    int j = va_arg(args, int);
+    
+    sum = n + j;
+    
+}
