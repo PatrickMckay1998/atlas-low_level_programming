2a78fdc6275815d3d90474f447061ea5206f99d4
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-24 10:18:17 -0500

    Moved the malloc to the top of the program after initializing variables

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -6,7 +6,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 int len1, len2;
 char *cpy_name[], *cpy_owner[];
-  
+
+cpy_name = malloc(sizeof(name) + 1);
+cpy_owner = malloc(sizeof(owner) + 1);
+
 len1 = 0;
   
   while(name[len1])
@@ -20,8 +23,5 @@ len1 = 0;
     cpy_owner[len2] = owner[len2];
     len2++;
   } /* obtaines length of owner and copies it to cpy_owner */
-  
-cpy_name = malloc(sizeof(name) + 1);
-cpy_owner = malloc(sizeof(owner) + 1);
 
 }
