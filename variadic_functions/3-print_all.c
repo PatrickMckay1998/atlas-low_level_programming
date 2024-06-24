fb000364ad1d24eae3678f8bc101d14b217fd720
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-24 00:23:43 -0500

    Good push, try again tomorrow

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+void print_all(const char * const format, ...)
+{
+    va_list args;
+
+    va_start(args, format);
+
+    nico = 0;
+
+    while (nico < format)
+    { 
+        if(va_arg(args, format) = )
+        nico++;
+    }
+}
+
+/* Whiteboard this one tomorrow bro, great job and lets keep up the grind */
