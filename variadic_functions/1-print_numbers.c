f589ee4e0bfd36687c72b8c66703629edc21110e
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-23 22:17:31 -0500

    Fixed mistakes

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,3 +1,22 @@
 #include <stdio.h> 
 #include <stdarg.h>
 
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+    va_list args;
+
+    va_start(args, n);
+
+    for(b = 0; b <= n -1; b++)
+    {
+        i = va_arg(args, int)
+        if(separator == 'NULL')
+        {
+            b++;
+        }
+        printf("%d", i);
+    }
+    
+    return (0);
+
+}
