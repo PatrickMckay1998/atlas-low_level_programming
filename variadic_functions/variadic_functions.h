3c38f8623a095e361437406e3785a66fc38fb1aa
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-23 22:16:27 -0500

    First attempt at the prigram

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -3,5 +3,24 @@
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+    va_list args;
+
+    va_start(args, n);
+
+    for(b = 0; b <= n -1; b++)
+    {
+        i = va_arg(args, int)
+        if(separator == 'NULL')
+        {
+            b++;
+        }
+        printf("%d", i);
+    }
+    
+    return (0);
+
+}
 
 #endif
