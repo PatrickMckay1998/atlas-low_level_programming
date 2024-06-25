314ade0d89be3439d5ba6c346a8bf037034f0f29
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-24 22:10:12 -0500

    Getting the betty score

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,8 +1,21 @@
 #include <stdio.h> 
 #include <stdarg.h>
 
+/**
+ * print_numbers - Prints numbers, followed by a separator
+ * @separator: The string to be printed between numbers
+ * @n: The number of integers passed to the function
+ *
+ * Description: This function takes a variable number of integers and prints them
+ * separated by the given separator string. If the separator is NULL, the numbers
+ * are printed without any separator. After printing all the numbers, a newline
+ * character is printed.
+ *
+ * Return: This function does not return a value.
+ */
+
 void print_numbers(const char *separator, const unsigned int n, ...)
-{      
+{
 unsigned int b, j;
 
     va_list args;
@@ -14,7 +27,7 @@ unsigned int b, j;
         j = va_arg(args, int);
 
         printf("%d", j);
-        
+
         if (separator != NULL && b < n -1)
         {
           printf("%s", separator);
@@ -24,7 +37,7 @@ unsigned int b, j;
 
     va_end(args);
     printf("\n");
-    
+
     return;
 
 }
