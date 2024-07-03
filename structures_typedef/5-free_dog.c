aa658609d9bbfe00d803f07ce28b9f5272091494
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-07-03 13:29:09 -0500

    Add description

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -2,6 +2,17 @@
 #include <stdlib.h>
 #include "dog.h"
 
+/**
+ * free_dog - Frees memory allocated for a dog
+ * @d: Pointer to a dog_t struct to be freed
+ *
+ * Description: This function frees the memory allocated for the name and owner
+ * members of a dog_t struct. If the age of the dog is 0, it prints 0 before
+ * freeing the memory.
+ *
+ * Return: This function does not return a value.
+ */
+
 void free_dog(dog_t *d)
 {
 
