cadde4f71f39ddb122f62a6b78a04f976cbcbcb7
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-07-04 14:07:29 -0500

    updated main and created program

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+list_t *add_node(list_t **head, const char *str)
+{
+    /*Create a new node*/
+    struct list_t new_node = (struct list_t*) malloc(sizeof(struct list_t));
+    /*Add the data*/
+    new_node->str = str;
+    
+    new_node->next = (*head);
+
+    (*head) = new_node;
+}
