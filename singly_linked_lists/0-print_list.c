d0b3ff69297ab84922cb9ba8a5a2b83884c12cfe
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-07-03 22:32:41 -0500

    Created lists.h and first file.

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,2 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
 
+/* lists.h contains the prototype for the list_t */
+
+size_t print_list(const list_t *h)
+{
+    list_t *temp = h;
+
+    while (temp != NULL)
+    {
+        printf("%s", temp.str);
+        temp = temp.next;
+    }
+    printf("\n");
+}
