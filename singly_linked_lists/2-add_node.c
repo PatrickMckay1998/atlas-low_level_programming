ae4711057a585e41ec9be44efd60dd6d53599483
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-07-04 14:17:48 -0500

    Adding a const before struct, added pointer to str

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -4,10 +4,10 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-    /*Create a new node*/
-    struct list_t* new_node = (struct list_t*) malloc(sizeof(struct list_t));
-    /*Add the data*/
-    new_node->str = str;
+    
+    const struct list_t* new_node = (struct list_t*) malloc(sizeof(struct list_t));
+    
+    new_node->str = *str;
     
     new_node->next = (*head);
 
