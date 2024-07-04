65ec49e92344e108efcf6e489680832c4acf6d3a
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-07-04 14:34:33 -0500

    Removed the struct

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -2,14 +2,16 @@
 #include <stdlib.h>
 #include "lists.h"
 
+
 list_t *add_node(list_t **head, const char *str)
 {
+    list_t* new_node = (list_t*) malloc(sizeof(list_t));
     
-    const struct list_t* new_node = (struct list_t*) malloc(sizeof(struct list_t));
+    new_node->str = str;
     
-    new_node->str = *str;
-    
-    new_node->next = (*head);
+    new_node->next = (**head);
 
-    (*head) = new_node;
+    (**head) = new_node;
+
+    return(&head);
 }
