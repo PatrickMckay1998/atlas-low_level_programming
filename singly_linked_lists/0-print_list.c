ec3e38f4151f09b0942b43649bce31e37fcec92f
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-07-03 23:14:17 -0500

    Fixed for Betty

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -2,30 +2,41 @@
 #include <stdlib.h>
 #include "lists.h"
 
-/* lists.h contains the prototype for the list_t */
+/*
+* @temp: This is temporary variable for h
+* @mia: She contains the number of elements to return
+* 
+* Description: This program calls in a struct with the pointer
+* variable 'h' which contains a string and unsigned integer. 
+* the program iterates through the nodes and prints our the data stored on 
+* the node. 
+*
+* Return - The number of elements which is stored in the variable mia
+*
+ */
 
 size_t print_list(const list_t *h)
 {
-    const list_t *temp = h;
+const list_t *temp = h;
 
-    unsigned int mia;
+unsigned int mia;
 
-    mia = 0;
+mia = 0;
 
-    while (temp != NULL && mia != temp->len)
-    {   
-        /*if (temp->str ==)
-        {
-            printf("[0]");
-            printf("\n");
-        } 
-        */
-        printf("[%d] %s\n", temp->len, temp->str);
-        mia++;
-        temp = temp->next;
-    }
+while (temp != NULL && mia != temp->len)
+{
+/*if (temp->str ==)
+{
+printf("[0]");
+printf("\n");
+}
+*/
+printf("[%d] %s\n", temp->len, temp->str);
+mia++;
+temp = temp->next;
+}
 
 
 
-    return(mia);
+return(mia);
 }
