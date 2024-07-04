c64375f53aa82c1dd609a26217ccb999ca05af23
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-07-03 23:26:55 -0500

    Fixed for betty

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -10,7 +10,8 @@
 * each node and uses mia to take in the number of elements 
 * will retrun the numer of elements stored in mia
 *
-* Return: Return the number of elements contained in mia*/
+* Return: Return the number of elements contained in mia
+*/
 
 size_t list_len(const list_t *h)
 {
