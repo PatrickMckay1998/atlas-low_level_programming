34db63c7ba6d0e25da695b711d8308c336a47aad
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-07-03 23:26:18 -0500

    Added Betty

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -2,18 +2,28 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/*
+* @pat: This is what is used to start goint into the linked list
+* @mia: She contains the number of elements
+*
+* Description: This program takes the struct H and iterates through
+* each node and uses mia to take in the number of elements 
+* will retrun the numer of elements stored in mia
+*
+* Return: Return the number of elements contained in mia*/
+
 size_t list_len(const list_t *h)
 {
-    const list_t *pat = h;
-    unsigned int mia;
+const list_t *pat = h;
+unsigned int mia;
 
-    mia = 0;
-    
-    while (pat != NULL)
-    {
-        mia++;
-        pat = pat->next;
-    }
+mia = 0;
 
-    return (mia);
+while (pat != NULL)
+{
+mia++;
+pat = pat->next;
+}
+
+return (mia);
 }
