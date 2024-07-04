6208db6b757c3bc0a4e3653fddfecf9aae96f45d
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-07-04 17:24:18 -0500

    added list_t to temp in malloc

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -11,7 +11,7 @@ list_t *add_node_end(list_t **head, const char *str)
     
     ptr = head;
     
-    temp = (list_t**) malloc(sizeof(list_t));
+    list_t temp = (list_t**) malloc(sizeof(list_t));
 
     str2 = strdup(str);
 
