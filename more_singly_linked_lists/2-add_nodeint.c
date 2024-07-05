8a61e98a98e62550af119703b6a0e5a16ceb9927
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-07-04 20:16:04 -0500

    Added ** to end of listint_t

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -8,7 +8,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
     listint_t **head_ref = head;
 
 
-    listint_t *new_node = (listint_t) malloc (sizeof(listint_t));
+    listint_t** new_node = (listint_t**) malloc (sizeof(listint_t));
     /* create a new node to be added */
 
     /* Add data to new node */
