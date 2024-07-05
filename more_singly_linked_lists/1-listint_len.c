b6ca0d8600a37f8822cb07d5a4d0cfd1d5f50019
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-07-04 19:44:36 -0500

    Updated h file and created file with program

---
+++ more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+size_t listint_len(const listint_t *h)
+{
+    listint_t *pat = h;
+    int mia;
+
+    mia = 0;
+
+    while(pat != NULL)
+    {
+        mia++;
+        pat = pat->next;
+    }
+
+    return (mia);
+
+}
