5e348cf59c514e5d7ce50d4ca457db80b22d819a
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-07-04 20:20:30 -0500

    Comment out old code and try again with new

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -5,19 +5,24 @@
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
+    
+
+
+
+
+
+    
+    return(&head_ref);
+/*
     listint_t **head_ref = head;
 
 
     listint_t** new_node = (listint_t**) malloc (sizeof(listint_t));
-    /* create a new node to be added */
-
-    /* Add data to new node */
+    
     new_node->n = const int n;
 
     new_node->next = (**head_ref);
 
     (**head_ref) = new_node;
-
-    return(&head_ref);
-
+    */
 }
