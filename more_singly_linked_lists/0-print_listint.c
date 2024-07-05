416bb88a3f642bb9e0799b847f2e8817f1c30779
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-07-04 19:01:30 -0500

    Wrote out program to print each element in the list

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -1,1 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+size_t print_listint(const listint_t *h)
+{
+    const listint_t *pat = h;
+
+    int mia;
+
+    mia = 0;
+
+    while (pat != NULL)
+    {
+        printf("%d", pat->int);
+        mia++;
+        pat = pat->next;
+    }
+    return (mia);
+
+}
