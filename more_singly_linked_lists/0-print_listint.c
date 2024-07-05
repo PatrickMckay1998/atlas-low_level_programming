316b02e56c5a80f6075d8486b7ada7d5708853af
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-07-04 19:09:09 -0500

    Fixed code for betty

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -2,20 +2,31 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/*
+* @pat: pat is used to access the linked list
+* @mia: She contains the number of elements to later return
+*
+* Description: This program takes the linked list and prints out the data 
+* stored inside of the nodes and returns the number of elements
+*
+* Return: returns the number stored in mia
+*
+*/
+
 size_t print_listint(const listint_t *h)
 {
-    const listint_t *pat = h;
+const listint_t *pat = h;
 
-    int mia;
+int mia;
 
-    mia = 0;
+mia = 0;
 
-    while (pat != NULL)
-    {
-        printf("%d\n", pat->n);
-        mia++;
-        pat = pat->next;
-    }
-    return (mia);
+while (pat != NULL)
+{
+printf("%d\n", pat->n);
+mia++;
+pat = pat->next;
+}
+return (mia);
 
 }
