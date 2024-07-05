7e3e2099926b27983b9d66693635c6541c1a9e18
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-07-04 19:58:21 -0500

    Updated lists.h created new program file and wrote program using previous code

---
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+    listint_t **new_node = (listint_t**) malloc (sizeof(listint_t));
+    /* create a new node to be added */
+
+    /* Add data to new node */
+    **new_node->n = const int n;
+
+    **new_node->next = (**head);
+
+    (**head) = **new_node;
+
+    return(&head);
+
+}
