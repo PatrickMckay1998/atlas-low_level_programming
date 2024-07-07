cdb0d0690aa2ccad57fc87fd289b1c764540211f
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-07-06 23:19:26 -0500

    Crack at new code

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -4,7 +4,25 @@
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-    listint_t** new_node = (listint_t**) malloc(sizeof(listint_t));
+    listint_t* new_node = (listint_t*) malloc(sizeof(listint_t));
+
+    list_t *last = head_ref;
+
+    new_node->n = n;
+
+    new_node->next = NULL;
+
+    if (*head_ref == NULL)
+    {
+        *head_ref = new_node;
+        return;
+    }
+
+    while (last->next != NULL)
+    last = last->next;
+
+    last->next = new_node;
+    return;
 
     
 }
--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -7,11 +7,11 @@ list_t *add_node_end(list_t **head, const char *str)
 {
     const char* str2;
     
-    list_t **ptr;
+    list_t *ptr;
     
     ptr = head;
     
-    list_t** temp = (list_t**) malloc(sizeof(list_t));
+    list_t* temp = (list_t*) malloc(sizeof(list_t));
 
     str2 = strdup(str);
 
