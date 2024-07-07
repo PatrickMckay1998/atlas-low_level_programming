bac37c724466bc5145188ff059eafe09f1fc864f
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-07-06 21:55:30 -0500

    Removed all ** from malloc statement

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -7,7 +7,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
     listint_t **head_ref = head;
 
-    listint_t new_node = (listint_t**) malloc (sizeof(listint_t));
+    listint_t new_node = (listint_t) malloc (sizeof(listint_t));
     
     new_node->n = const int n;
 
