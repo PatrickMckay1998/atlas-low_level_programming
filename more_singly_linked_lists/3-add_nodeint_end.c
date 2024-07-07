dd398af6de4fa9360ab831e73fd96b616e9f9468
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-07-06 23:22:33 -0500

    Duh

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -6,7 +6,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
     listint_t* new_node = (listint_t*) malloc(sizeof(listint_t));
 
-    listint_t* last = *head_ref;
+    listint_t* last = *head;
 
     new_node->n = n;
 
@@ -14,7 +14,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
     if (*head_ref == NULL)
     {
-        *head_ref = new_node;
+        *head = new_node;
         return;
     }
 
