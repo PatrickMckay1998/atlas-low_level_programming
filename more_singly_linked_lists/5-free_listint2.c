c6f6bbfa11f87e7492f933af21c5c3f26d83fea6
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-07-07 12:31:12 -0500

    Trying something different

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -6,6 +6,8 @@ void free_listint2(listint_t **head)
 {
     /* listint_t* temp; */
 
+    listint_t* temp = (listint_t*) malloc(sizeof(listint_t));
+
     while (*head != NULL)
     {
         *head = temp;
