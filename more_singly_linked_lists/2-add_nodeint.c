2bb8c5ea5a92b7a70a154e3a72ee7959e9665fca
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-07-06 22:37:43 -0500

    Have I finally realized the errors or my way?

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -7,16 +7,14 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
     const int temp_n = n;
 
-    listint_t **head_ref = head;
-
     listint_t* new_node = (listint_t*) malloc (sizeof(listint_t));
     
     new_node->n = temp_n;
 
-    new_node->next = (**head_ref);
+    new_node->next = (head);
 
-    (**head_ref) = new_node;
+    (head) = new_node;
 
-    return(&head_ref);
+    return(&head);
 
 }
