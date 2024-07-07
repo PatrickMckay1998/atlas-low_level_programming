23bdee7afc1d1b28f92893339f165cb6b835c915
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-07-07 08:45:07 -0500

    Did some research and found a way

--- more_singly_linked_lists/4-free_listint.c
+++ more_singly_linked_lists/4-free_listint.c
@@ -4,8 +4,13 @@
 
 void free_listint(listint_t *head)
 {
-    listint_t* temp = (listint_t*) malloc(sizeof(listint_t));
+    listint_t* temp;
 
-    free(temp);
-    return (0);
+    while(head != NULL)
+    {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+    return(0);
 }
