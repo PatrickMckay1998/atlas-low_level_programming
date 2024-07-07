3f47b7b78a7f1b0789e4749f822944626562032e
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-07-06 21:50:07 -0500

    Removed ** from new node

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -6,23 +6,17 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
     
-
-
-
-
-
-    
-    return(&head_ref);
-/*
     listint_t **head_ref = head;
 
-
-    listint_t** new_node = (listint_t**) malloc (sizeof(listint_t));
+    listint_t new_node = (listint_t**) malloc (sizeof(listint_t));
     
     new_node->n = const int n;
 
     new_node->next = (**head_ref);
 
     (**head_ref) = new_node;
-    */
+
+
+    return(&head_ref);
+
 }
