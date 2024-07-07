476ae1a0177b0398b081045881c624e2b4d0d231
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-07-06 22:31:11 -0500

    Trying a temp n to see if its just being picky

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -5,11 +5,13 @@
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
+    const int temp_n = n
+
     listint_t **head_ref = head;
 
     listint_t** new_node = (listint_t**) malloc (sizeof(listint_t));
     
-    new_node->n = n;
+    new_node->n = temp_n;
 
     new_node->next = (**head_ref);
 
