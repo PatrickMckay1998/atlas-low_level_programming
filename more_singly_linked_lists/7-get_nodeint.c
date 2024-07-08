a917e4830f0b972622775e52623c7c49bb02fa82
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-07-07 20:46:36 -0500

    Created program file 7 and wrote a program

---
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+    /* So we need to iterate through the linked list until
+     the nth index is reached and then return the node */
+
+    listint_t* temp = head;
+
+    unsigned int mia;
+
+    mia = 0;
+
+    while(mia <= index)
+    {
+        temp->next;
+        mia++;
+        /* if (mia <= index)
+        {
+            temp->next
+            mia++;
+        }
+        */
+    }
+
+    return (temp);
+
+}
