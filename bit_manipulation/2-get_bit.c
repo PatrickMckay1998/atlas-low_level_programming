b95cd654ad05b6f7c1d6b4c0353cc8d0798098f8
patrick mckay <patrick.mckay@atlasschool.com>
2024-07-08 21:55:44 -0500

    Create program file 2 and write out program

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+int get_bit(unsigned int n, unsigned int index)
+{
+    unsigned int mask = 1UL << index;
+
+    if (index >= sizeof(unsigned long int) * 8)
+    {
+        return (-1)
+    }
+
+    if (n & mask)
+    {
+        return (1);
+    }
+    else 
+    {
+        return(0);
+    }
+
+}
