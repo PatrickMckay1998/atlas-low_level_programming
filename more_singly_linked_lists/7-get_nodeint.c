2dcce70ceb7866c77a777fba815c9086d5bbab37
patrick mckay <patrick.mckay@atlasschool.com>
2024-07-08 22:52:17 -0500

    mon

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -4,49 +4,16 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-    /* So we need to iterate through the linked list until
-     the nth index is reached and then return the node */
+    unsigned int i = 0;
 
-    listint_t* temp = head;
-
-    unsigned int mia;
-
-    mia = 0;
-
-    while(mia <= index)
+    while (head != NULL)
     {
-        temp = temp->next;
-        mia++;
-        /* if (mia <= index)
-        {
-            temp->next
-            mia++;
-        }
-        */
+        if (i == index)
+            return (head);
+        head = head->next;
+        i++;
     }
 
-    return (temp);
-
+    return (NULL);
 }
 
-int main(void)
-{
-    listint_t *head;
-    listint_t *node;
-
-    head = NULL;
-    add_nodeint_end(&head, 0);
-    add_nodeint_end(&head, 1);
-    add_nodeint_end(&head, 2);
-    add_nodeint_end(&head, 3);
-    add_nodeint_end(&head, 4);
-    add_nodeint_end(&head, 98);
-    add_nodeint_end(&head, 402);
-    add_nodeint_end(&head, 1024);
-    print_listint(head);
-    node = get_nodeint_at_index(head, 5);
-    printf("%d\n", node->n);
-    print_listint(head);
-    free_listint2(&head);
-    return (0);
-}
