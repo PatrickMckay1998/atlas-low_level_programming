9afffa25e45f1a4540661e97a457948e6271db5c
patrick mckay <patrick.mckay@atlasschool.com>
2024-07-08 20:53:02 -0500

    Create program file 1

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+void print_binary(unsigned long int n);
+
+int main(void)
+{
+    print_binary(0);
+    printf("\n");
+    print_binary(1);
+    printf("\n");
+    print_binary(98);
+    printf("\n");
+    print_binary(1024);
+    printf("\n");
+    print_binary((1 << 10) + 1);
+    printf("\n");
+    return (0);
+}
+
+void print_binary(unsigned long int n)
+{   
+    char* binary;
+
+    int i, mia = 0;
+
+    while(n) {
+        binary[i++] = '0' + (n & 1);
+        n >>= 1;
+    }
+    binary[i] = '\0';
+    return (binary - 1);
+}
