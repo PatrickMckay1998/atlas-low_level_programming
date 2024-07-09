faef5483250b68c3163a5d28de3d1d8ae011da35
patrick mckay <patrick.mckay@atlasschool.com>
2024-07-08 22:58:18 -0500

    mon

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
         current = current->next;
     }
 
-    if (current == NULL || current->next == NULL && i < idx - 1)
+    if (current == NULL || (current->next == NULL && i < idx - 1))
     {
         free(new_node);
         return (NULL);
