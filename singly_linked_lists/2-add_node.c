b76140b5b44941136428449189d0341e9f4da2d1
patrick mckay <patrick.mckay@atlasschool.com>
2024-07-09 20:45:45 -0500

    One big push

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -2,55 +2,38 @@
 #include <stdlib.h>
 #include "lists.h"
 
-list_t *add_node(list_t **head, const char *str);
-
-int main(void)
-{
-    list_t *head;
-
-    head = NULL;
-    add_node(&head, "Alexandro");
-    add_node(&head, "Asaia");
-    add_node(&head, "Augustin");
-    add_node(&head, "Bennett");
-    add_node(&head, "Bilal");
-    add_node(&head, "Chandler");
-    add_node(&head, "Damian");
-    add_node(&head, "Daniel");
-    add_node(&head, "Dora");
-    add_node(&head, "Electra");
-    add_node(&head, "Gloria");
-    add_node(&head, "Joe");
-    add_node(&head, "John");
-    add_node(&head, "John");
-    add_node(&head, "Josquin");
-    add_node(&head, "Kris");
-    add_node(&head, "Marine");
-    add_node(&head, "Mason");
-    add_node(&head, "Praylin");
-    add_node(&head, "Rick");
-    add_node(&head, "Rick");
-    add_node(&head, "Rona");
-    add_node(&head, "Siphan");
-    add_node(&head, "Sravanthi");
-    add_node(&head, "Steven");
-    add_node(&head, "Tasneem");
-    add_node(&head, "William");
-    add_node(&head, "Zee");
-    print_list(head);
-    return (0);
-}
-
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t* new_node = (list_t*) malloc(sizeof(list_t));
-    
-    new_node->str = (*head)->str;
-    /* new_node->str = strdup(str) */
-    
-    new_node->next = (*head);
+   list_t *new_node;
+    char *new_str;
+    unsigned int len = 0;
 
-    (*head) = new_node;
+    if (str == NULL)
+    {
+        return (NULL);
+    }
+    new_node = malloc(sizeof(list_t));
+    if (new_node == NULL)
+    {
+        return (NULL);
+    }
 
-    return (*head);
+    new_str = strdup(str);
+    
+    if (new_str == NULL)
+    {
+        free(new_node);
+        return (NULL);
+    }
+
+    while (str[len])
+    {
+        len++;
+    }
+    new_node->str = new_str;
+    new_node->len = len;
+    new_node->next = *head;
+    *head = new_node;
+    
+    return (new_node);
 }
--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -5,24 +5,45 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-    const char* str2;
-    
-    list_t *ptr;
-    
-    ptr = head;
-    
-    list_t* temp = (list_t*) malloc(sizeof(list_t));
+    list_t *new_node;
+    list_t *temp = *head;
+    char *new_str;
+    unsigned int len = 0;
 
-    str2 = strdup(str);
+    if (str == NULL)
+        return (NULL);
 
-    temp->str = str2;
-    temp->next = NULL;
+    new_node = malloc(sizeof(list_t));
+    if (new_node == NULL)
+        return (NULL);
 
-    while(ptr->next != NULL)
+    new_str = strdup(str);
+    if (new_str == NULL)
     {
-        ptr = ptr->next;
+        free(new_node);
+        return (NULL);
     }
-    ptr->next = temp;
-    return(&temp);
+
+    while (str[len])
+    {
+        len++;
+    }
+    
+    new_node->str = new_str;
+    new_node->len = len;
+    new_node->next = NULL;
+
+    if (*head == NULL)
+    {
+        *head = new_node;
+    }
+    else
+    {
+        while (temp->next)
+            temp = temp->next;
+        temp->next = new_node;
+    }
+
+    return (new_node);
 }
 
