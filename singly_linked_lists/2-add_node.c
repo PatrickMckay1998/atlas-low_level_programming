c36052c03cf7233c6a7a8600d9ec8743f995ed67
patrick mckay <patrick.mckay@atlasschool.com>
2024-07-09 20:19:34 -0500

    Fixed references to pointers

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -2,17 +2,57 @@
 #include <stdlib.h>
 #include "lists.h"
 
+list_t *add_node(list_t **head, const char *str);
+
+int main(void)
+{
+    list_t *head;
+
+    head = NULL;
+    add_node(&head, "Alexandro");
+    add_node(&head, "Asaia");
+    add_node(&head, "Augustin");
+    add_node(&head, "Bennett");
+    add_node(&head, "Bilal");
+    add_node(&head, "Chandler");
+    add_node(&head, "Damian");
+    add_node(&head, "Daniel");
+    add_node(&head, "Dora");
+    add_node(&head, "Electra");
+    add_node(&head, "Gloria");
+    add_node(&head, "Joe");
+    add_node(&head, "John");
+    add_node(&head, "John");
+    add_node(&head, "Josquin");
+    add_node(&head, "Kris");
+    add_node(&head, "Marine");
+    add_node(&head, "Mason");
+    add_node(&head, "Praylin");
+    add_node(&head, "Rick");
+    add_node(&head, "Rick");
+    add_node(&head, "Rona");
+    add_node(&head, "Siphan");
+    add_node(&head, "Sravanthi");
+    add_node(&head, "Steven");
+    add_node(&head, "Tasneem");
+    add_node(&head, "William");
+    add_node(&head, "Zee");
+    print_list(head);
+    return (0);
+}
 
 list_t *add_node(list_t **head, const char *str)
-{
+{   
+    const char* str_2 = str;
+
     list_t* new_node = (list_t*) malloc(sizeof(list_t));
     
-    new_node->str = str;
+    new_node->str = str_2;
     /* new_node->str = strdup(str) */
     
-    new_node->next = (**head);
+    new_node->next = (*head);
 
-    (**head) = new_node;
+    (*head) = new_node;
 
     return(&head);
 }
