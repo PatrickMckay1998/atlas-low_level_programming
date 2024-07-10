c0ebe11e26ca718feda63b9bcae4b71d7ee9378f
patrick mckay <patrick.mckay@atlasschool.com>
2024-07-09 20:31:10 -0500

    added parenth and ptr to head

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -47,7 +47,7 @@ list_t *add_node(list_t **head, const char *str)
 
     list_t* new_node = (list_t*) malloc(sizeof(list_t));
     
-    new_node->str = head->str;
+    new_node->str = (*head)->str;
     /* new_node->str = strdup(str) */
     
     new_node->next = (*head);
