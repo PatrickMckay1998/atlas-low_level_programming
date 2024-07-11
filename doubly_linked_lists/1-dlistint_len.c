7131942a4f67edb41e448674409a48c07f9bcf7c
patrick mckay <patrick.mckay@atlasschool.com>
2024-07-11 13:57:59 -0500

    Create new program file 1

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+size_t dlistint_len(const dlistint_t *h)
+{
+    const dlistint_t* temp = *h;
+
+    int mia = 0;
+
+    while (temp != NULL)
+    {
+        mia++;
+        temp = temp->next;
+    }
+
+    return (mia);
+
+}
