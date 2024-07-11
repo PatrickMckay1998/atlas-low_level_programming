0edb54533e2474206df13d56a8454522e546c99c
patrick mckay <patrick.mckay@atlasschool.com>
2024-07-11 13:29:47 -0500

    removed iff null statement

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -31,19 +31,21 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-     const dlistint_t *temp = h;
+    const dlistint_t *temp = h;
 
     int mia = 0;
 
+    /*
     if (temp->next == NULL)
     {
         return (0);
     }
+    */
 
-    else if (temp->next != NULL)
+    while (temp->next != NULL)
     {   
         mia++;
-        printf("%d", mia);
+        printf("%d\n", temp->n); 
         temp = temp->next;
     }
     return (mia);
