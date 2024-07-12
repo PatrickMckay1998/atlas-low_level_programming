8acdec2fa0f5d866c78bc698c2e1ecafa10fc49c
patrick mckay <patrick.mckay@atlasschool.com>
2024-07-12 17:03:39 -0500

    changes

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -5,6 +5,15 @@
 
 void free_dlistint(dlistint_t *head)
 {   
-    free(head);
+    dlistint_t* temp;
+
+    if (head != NULL)
+    {
+        temp = head;
+        head = head->next;
+        free(temp->n);
+        free(temp);
+    }
+    
     return;
 }
