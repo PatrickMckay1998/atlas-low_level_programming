901a364a037c0a25f07934b42750e2c66f7e3413
patrick mckay <patrick.mckay@atlasschool.com>
2024-07-11 22:08:08 -0500

    fixed if

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -11,7 +11,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
     if (*head == NULL)
     {
         *head = new_node;
-        return;
+        return(new_node);
     }
         new_node->n = n;
         new_node->next = (*head);
