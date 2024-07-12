118f819cb724b4d2908db3f13fd438b6f96f0fbb
patrick mckay <patrick.mckay@atlasschool.com>
2024-07-11 22:12:55 -0500

    Add both blocks of code, comment one out, moving on

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -7,16 +7,25 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 
     dlistint_t* new_node = (dlistint_t*) malloc(sizeof(dlistint_t));
+    
+    while (new_node != NULL)
+    {
+        new_node->n = n;
+        new_node->next = (*head);
+        (*head) = new_node;
+    }
 
+    /*
     if (*head == NULL)
     {
         *head = new_node;
         return(new_node);
     }
+
         new_node->n = n;
         new_node->next = (*head);
         (*head) = new_node;
-    
+    */
     return (new_node);
 }
 /*
