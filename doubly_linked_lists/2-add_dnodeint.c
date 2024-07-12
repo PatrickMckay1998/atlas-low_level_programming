75673a0936a3267e73bb5747b96584a745825884
patrick mckay <patrick.mckay@atlasschool.com>
2024-07-12 09:51:02 -0500

    Did more research and rewrote the code

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -3,18 +3,38 @@
 #include <stdio.h>
 #include "lists.h"
 
+/*
+* https://www.geeksforgeeks.org/introduction-and-insertion-in-a-doubly-linked-list/
+* 
+* This is the website used in helping me understand doubly linked lists
+* 
+* 
+*/
+
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-
     dlistint_t* new_node = (dlistint_t*) malloc(sizeof(dlistint_t));
     
-    while (new_node != NULL)
+    new_node->n = n;
+    
+    new_node->next = (*head);
+    new_node->prev = NULL;
+
+    if ((*head) != NULL)
+    {
+        (*head)->prev = new_node;
+    }
+
+    (*head) = new_node;
+
+
+    /*while (new_node != NULL)
     {
         new_node->n = n;
         new_node->next = (*head);
         (*head) = new_node;
     }
-
+    */
     /*
     if (*head == NULL)
     {
