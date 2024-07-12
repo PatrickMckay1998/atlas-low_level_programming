a02d566f22c69c70da81f2ebb0469b988c33c353
patrick mckay <patrick.mckay@atlasschool.com>
2024-07-11 21:38:47 -0500

    Added previous code block into a while loop

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -8,9 +8,12 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
     dlistint_t* new_node = (dlistint_t*) malloc(sizeof(dlistint_t));
 
-    new_node->n = n;
-    new_node->next = (*head);
-    (*head) = new_node;
+    while (new_node != NULL)
+    {   
+        new_node->n = n;
+        new_node->next = (*head);
+        (*head) = new_node;
+    }
     return (new_node);
 }
 /*
