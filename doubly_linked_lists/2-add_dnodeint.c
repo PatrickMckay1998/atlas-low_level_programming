9e37830f521f38c93b6c38709951134e4d4596a9
patrick mckay <patrick.mckay@atlasschool.com>
2024-07-11 21:44:17 -0500

    Trying an if statment

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -8,7 +8,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
     dlistint_t* new_node = (dlistint_t*) malloc(sizeof(dlistint_t));
 
-    while (new_node != NULL)
+    if (new_node != NULL)
     {   
         new_node->n = n;
         new_node->next = (*head);
