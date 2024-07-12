28eb13412f4ab47db31b2fab5f4830d516bcdb2d
patrick mckay <patrick.mckay@atlasschool.com>
2024-07-12 10:18:40 -0500

    Removed if statement from while loop

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -15,17 +15,15 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
     dlistint_t* new_node = (dlistint_t*) malloc(sizeof(dlistint_t));
     
-    while(head)
+    while((*head) != NULL)
     {
         new_node->n = n;
     
         new_node->next = (*head);
         new_node->prev = NULL;
 
-        if ((*head) != NULL)
-        {
-            (*head)->prev = new_node;
-        }
+
+        (*head)->prev = new_node;
 
         (*head) = new_node;
     }
