f2ce369b0a137957ca7893f75586f0fea9dd8254
patrick mckay <patrick.mckay@atlasschool.com>
2024-07-11 22:06:55 -0500

    removed while and added if to check if the list is empty

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -8,12 +8,15 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
     dlistint_t* new_node = (dlistint_t*) malloc(sizeof(dlistint_t));
 
-    while (head)
+    if (*head == NULL)
     {
+        *head = new_node;
+        return;
+    }
         new_node->n = n;
         new_node->next = (*head);
         (*head) = new_node;
-    }
+    
     return (new_node);
 }
 /*
