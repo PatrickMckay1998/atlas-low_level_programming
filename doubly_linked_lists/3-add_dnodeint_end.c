75d934b8a5c70982b63d06c10e732fb19921f97d
patrick mckay <patrick.mckay@atlasschool.com>
2024-07-12 14:56:04 -0500

    Fixed what I broke

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -20,7 +20,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
     if ((*head) == NULL) /* if head is null then set new node as head */
     {
        new_node->prev = NULL; 
-       head = new_node;
+       (*head) = new_node;
        return (new_node);
     }
 
