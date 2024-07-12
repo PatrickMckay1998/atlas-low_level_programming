aa364bc1f4614bd52d3af3c3077d546642c6722d
patrick mckay <patrick.mckay@atlasschool.com>
2024-07-11 21:21:35 -0500

    Create program file 2, write basic code, no errors on my terminal

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,32 @@
+#include <stdlib.h> 
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+
+    dlistint_t* new_node = (dlistint_t*) malloc(sizeof(dlistint_t));
+
+    new_node->n = n;
+    new_node->next = (*head);
+    (*head) = new_node;
+    return (new_node);
+}
+
+int main(void)
+{
+    dlistint_t *head;
+
+    head = NULL;
+    add_dnodeint(&head, 0);
+    add_dnodeint(&head, 1);
+    add_dnodeint(&head, 2);
+    add_dnodeint(&head, 3);
+    add_dnodeint(&head, 4);
+    add_dnodeint(&head, 98);
+    add_dnodeint(&head, 402);
+    add_dnodeint(&head, 1024);
+    print_dlistint(head);
+    return (EXIT_SUCCESS);
+}
