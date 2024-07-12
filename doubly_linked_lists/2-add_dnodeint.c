e6084ada824e2c77e4254606b9fafc5391d52b9d
patrick mckay <patrick.mckay@atlasschool.com>
2024-07-11 21:50:24 -0500

    Changed condition from new node to head

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -8,7 +8,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
     dlistint_t* new_node = (dlistint_t*) malloc(sizeof(dlistint_t));
 
-    if (new_node != NULL)
+    if (head != NULL)
     {   
         new_node->n = n;
         new_node->next = (*head);
