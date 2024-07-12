5afc0099fea0e49cc93b73bcbe64101881be3c55
patrick mckay <patrick.mckay@atlasschool.com>
2024-07-12 11:06:59 -0500

    Fixed return

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -25,6 +25,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
         new_node->next = (*head);
         (*head) = new_node;
     
+    return (new_node);
+
 }
    
    /* Code using geeksforgeeks way of doing it
