e58966b1ecb65b22f5d2654d99d5e099ea3cea06
patrick mckay <patrick.mckay@atlasschool.com>
2024-07-12 13:24:21 -0500

    Fixed code

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -3,22 +3,38 @@
 #include <stdio.h>
 #include "lists.h"
 
+/* 
+* https://www.geeksforgeeks.org/introduction-and-insertion-in-a-doubly-linked-list/
+*
+* This is the website I used to help get the structure for my code
+*/
+
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t* next_node = (*head); /* Set next node to head*/
-
     dlistint_t* new_node = (dlistint_t*) malloc (sizeof(dlistint_t)); /* Create new node for linked list */
 
-    while (next_node != NULL)
+    dlistint_t* next_node = (dlistint_t*) malloc (sizeof(dlistint_t)); /* create next node to go through list */
+
+    new_node->n = n;
+    new_node->next = NULL;
+    
+    if ((*head) == NULL) /* if head is null then set new node as head */
     {
-        if (next_node == NULL)
-        {
-            new_node->n = n;
-            new_node->next = NULL;
-            next_node = new_node;
-        }
+       new_node->prev = NULL; 
+       (*head) = new_node;
+       return (new_node);
     }
+
+    while (next_node->next != NULL) /* while head doesnt equal null then move to the next */
+    { /* go through the list until the end, set next to = the new node
+        set the new node prev to point to next_node */
+        next_node = next_node->next;
+        next_node->next = new_node;
+        new_node->prev = next_node;
+    }
+    
     return (new_node);
+
 }
 /*
 int main(void)
