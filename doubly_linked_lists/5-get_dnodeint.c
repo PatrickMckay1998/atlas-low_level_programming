ff85951e27c0be46ca5420142ca4547c3a9177d2
patrick mckay <patrick.mckay@atlasschool.com>
2024-07-12 21:04:07 -0500

    Create program file 5, white board and try program

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+{
+    dlistint_t *current = head;
+    dlistint_t *next;
+
+    unsigned int i;
+
+    i = 0;
+
+    while (i <= index) /* while is less than or equal to index then move on to the next node */
+    {
+        next = current->next;
+        current = next;
+        i++;
+    }
+    
+    return (current);
+
+}
