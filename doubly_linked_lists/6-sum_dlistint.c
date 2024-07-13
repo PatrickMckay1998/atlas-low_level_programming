b3ad6fca16f0638c826e765ac59bfcac4a3b20d2
patrick mckay <patrick.mckay@atlasschool.com>
2024-07-12 21:31:22 -0500

    Create program file 6, white board and try out code

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+
+int sum_dlistint(distint_t *head)
+{
+    dlistint_t *current = head;
+    dlistint_t *next;
+
+    int sum;
+
+    sum = 0;
+
+    while (current != NULL)
+    {
+        sum += current->n;
+        next = current->next;
+        current = next;
+    }
+
+    return (sum);
+
+}
