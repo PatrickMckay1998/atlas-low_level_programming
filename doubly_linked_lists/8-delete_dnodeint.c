85689561bbcdbc7a0216c56915b62cfda0ac205d
patrick mckay <patrick.mckay@atlasschool.com>
2024-07-14 00:03:21 -0500

    added statment if del is head

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -29,10 +29,15 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
     {
         del->next->prev = del->prev; /* the prev of the node after del is set to the node before del */
     }
-    
-    if (del->prev != NULL)
+
+    else if (del->prev != NULL)
     {
         del->prev->next = del->next; /* the next of the node before del is set to the node after del */
+    }
+
+    if (del == (*head))
+    {
+        del->next->prev = NULL; /* Deletes head */
     }
 
     return(1);
