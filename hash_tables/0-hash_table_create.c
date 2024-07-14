f1df499edb6bea4e9f7280499d5ec68dd148a6c9
patrick mckay <patrick.mckay@atlasschool.com>
2024-07-14 15:13:59 -0500

    Attempt to write program

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -5,5 +5,10 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    
+    hash_table_t *hash_table = (hash_table_t*) malloc (sizeof(hash_table_t));
+    /* Create a new hash table, I think. */
+    hash_table->size = size;
+    /* Set the size of our new table to the size provided */
+    return (*hash_table);
+    /* Return the a pointer to the new hash_table */
 }
