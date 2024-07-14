a4fe9d33f7dd8d8ccb17a21733f1678696120a9e
patrick mckay <patrick.mckay@atlasschool.com>
2024-07-13 23:38:17 -0500

    Create program file 8, write prgram after research and white board

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+    dlistint_t* current = (*head);
+    dlistint_t* next, del;
+
+    unsigned int i = 0;
+
+    while (i != index) /* Loop through list until i = index */
+    {
+        next = current->next;
+        current = next;
+        i++;
+    }
+
+    current = del; /* After index is reached set current to del for better understanding */
+
+    del->next->prev = del->prev; /* the prev of the node after del is set to the node before del */
+    del->prev->next = del->next; /* the next of the node before del is set to the node after del */
+
+    return(1);
+
+}
