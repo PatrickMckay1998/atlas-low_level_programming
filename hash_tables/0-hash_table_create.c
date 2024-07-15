b4a69cd2998464caea3a51e5e1d6b2104a28ac44
patrick mckay <patrick.mckay@atlasschool.com>
2024-07-15 11:26:59 -0500

    Still not working

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "hash_tables.h"
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t *hash_table;
+    unsigned long int i;
+
+    /* Allocate memory for the hash table structure */
+    hash_table = malloc(sizeof(hash_table_t));
+    if (hash_table == NULL)
+        return (NULL);
+
+    /* Set the size of the hash table */
+    hash_table->size = size;
+
+    /* Allocate memory for the array of pointers to hash_node_t */
+    hash_table->array = calloc(size, sizeof(hash_node_t *));
+    if (hash_table->array == NULL)
+    {
+        free(hash_table); /* Free the allocated memory for the structure */
+        return (NULL);
+    }
+
+    /* Initialize each element of the array to NULL */
+    for (i = 0; i < size; i++)
+        hash_table->array[i] = NULL;
+
+    return (hash_table);
+}
