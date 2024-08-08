785d3b701962b5b4706d4dcb6523e5e0616db7fb
patrick mckay <patrick.mckay@atlasschool.com>
2024-08-08 14:54:57 -0500

    added prototype to file 0

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -1,5 +1,14 @@
-#ifndef SEARCH_ALGOS
-#define SEARCH_ALGOS
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+/* libraries to be used */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+/* prototypes */
 
 int linear_search(int *array, size_t size, int value);
 
