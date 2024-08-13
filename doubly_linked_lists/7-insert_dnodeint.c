deb18d887ce2d119c4032e49443da8f3f389b5b8
patrick mckay <patrick.mckay@atlasschool.com>
2024-08-13 18:30:58 -0500

    Update file with my new program from notes

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -3,43 +3,52 @@
 #include <string.h>
 #include "lists.h"
 
-/*
-* 
-* https://www.geeksforgeeks.org/introduction-and-insertion-in-a-doubly-linked-list/
-* 
-* This is the website I used that really helped out.
-*/
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t* new_node = (dlistint_t*) malloc (sizeof(dlistint_t));
+    dlistint_t *new_node, *current;
+    unsigned int i;
 
-    dlistint_t* current = (*h); /* Establish varialbes */
-    dlistint_t* next; /* Helps keep track of where we are in the list */
-    dlistint_t* prev_node; /* will be used to better understand adding new node later */
-
-    unsigned int i = 0; /* set int to unsiged to it doesnt throw a warning */
+    new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+    if (!new_node)
+    {
+        return (NULL);
+    }
 
     new_node->n = n;
 
-    while(i <= idx) /* move through list until idx is reached then leave loop */
+    if (idx == 0)
     {
-        next = current->next;
-        current = next;
-        i++;
+        new_node->next = *h;
+        new_node->prev = NULL;
+        if (*h)
+        {
+            (*h)->prev = new_node;
+        }
+        *h = new_node
+        return (new_node);
     }
 
-    prev_node = current; /* Set current node to prev for my understanding */
-
-    new_node->next = prev_node->next; /* set new node next to what prev was originally pointing to (Makes sens on my board) */
-    prev_node->next = new_node; /* Prev node now points to new node instead */
-    new_node->prev = prev_node; /* set the prev of our new node to point to the prev node */
-
-    if (new_node->next != NULL)
+    current = *h;
+    for (i = 0; current && i < idx - 1; i++)
     {
-        new_node->next->prev = new_node;
+        current = current ->next;
     }
-    
-    return (new_node); /* Return new node */
 
+    if (!current)
+    {
+        free(new_node);
+        return (NULL);
+    }
+
+    new_node->next = current->next;
+    new_node->prev = current;
+
+    if (current->next)
+    {
+        current->next->prev = new_node;
+    }
+
+    current->next = new_node;
+
+    return (new_node);
 }
