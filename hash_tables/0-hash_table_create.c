7a8384532b57e26ab7788da5e16206050f03a5cf
patrick mckay <patrick.mckay@atlasschool.com>
2024-08-13 16:42:50 -0500

    Add file description

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,5 +1,17 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_create - Creates a new hash table
+ * @size: The size of the hash table (number of buckets)
+ *
+ * Description: This function allocates memory for a hash table structure
+ *              and its internal array of pointers. It also initializes each
+ *              pointer in the array to NULL.
+ *
+ * Return: A pointer to the newly created hash table, or NULL if memory
+ *         allocation fails.
+ */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
 hash_table_t *hash_table;
