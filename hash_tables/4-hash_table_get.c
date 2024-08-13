b3b30fb303913edc336497d27375f555f991178f
patrick mckay <patrick.mckay@atlasschool.com>
2024-08-13 17:52:05 -0500

    Update for betty

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -9,27 +9,27 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    unsigned long int index;
-    hash_node_t *node;
+unsigned long int index;
+hash_node_t *node;
 
-    /* Check for invalid input */
-    if (ht == NULL || key == NULL || *key == '\0')
-        return (NULL);
+/* Check for invalid input */
+if (ht == NULL || key == NULL || *key == '\0')
+return (NULL);
 
-    /* Calculate the index for the key using the key_index function */
-    index = key_index((const unsigned char *)key, ht->size);
+/* Calculate the index for the key using the key_index function */
+index = key_index((const unsigned char *)key, ht->size);
 
-    /* Traverse the linked list at the computed index */
-    node = ht->array[index];
-    while (node != NULL)
-    {
-        /* If the key is found, return the associated value */
-        if (strcmp(node->key, key) == 0)
-            return (node->value);
+/* Traverse the linked list at the computed index */
+node = ht->array[index];
+while (node != NULL)
+{
+/* If the key is found, return the associated value */
+if (strcmp(node->key, key) == 0)
+return (node->value);
 
-        node = node->next;
-    }
+node = node->next;
+}
 
-    /* If the key wasn't found, return NULL */
-    return (NULL);
+/* If the key wasn't found, return NULL */
+return (NULL);
 }
