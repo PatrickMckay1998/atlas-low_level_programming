55a1d4aab0b677593796c6c31a0d636cca9b646e
patrick mckay <patrick.mckay@atlasschool.com>
2024-08-13 18:32:07 -0500

    Forgot to add ;

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -9,6 +9,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
     unsigned int i;
 
     new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+
     if (!new_node)
     {
         return (NULL);
@@ -24,11 +25,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
         {
             (*h)->prev = new_node;
         }
-        *h = new_node
+        *h = new_node;
         return (new_node);
     }
 
     current = *h;
+    
     for (i = 0; current && i < idx - 1; i++)
     {
         current = current ->next;
