f5d8a4adee5aac97d5d1e6e971a8572ac1ad1a51
patrick mckay <patrick.mckay@atlasschool.com>
2024-08-13 16:17:02 -0500

    Update file 2 with description for betty

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,5 +1,14 @@
 #include "hash_tables.h"
 
+/**
+ * key_index - Gives the index of a key for a hash table
+ * @key: The key (a string) to be hashed
+ * @size: The size of the hash table array
+ *
+ * Return: The index at which the key/value pair should be stored in the array
+ *         of the hash table
+ */
+
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 return (hash_djb2(key) % size);
