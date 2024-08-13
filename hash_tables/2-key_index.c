ccac1e949cd98e8cb712d61c0e036bece2753fb8
patrick mckay <patrick.mckay@atlasschool.com>
2024-08-13 16:06:57 -0500

    Fix for betty

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,9 +1,6 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "hash_tables.h"
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    return hash_djb2(key) % size;
+return hash_djb2(key) % size;
 }
