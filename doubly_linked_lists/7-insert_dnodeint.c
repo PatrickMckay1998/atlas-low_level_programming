5fac06a6505cee21a6426037856d8da290f40699
patrick mckay <patrick.mckay@atlasschool.com>
2024-08-13 18:38:19 -0500

    Added description

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -3,6 +3,15 @@
 #include <string.h>
 #include "lists.h"
 
+/**
+ * insert_dnodeint_at_index - Inserts a new node at a given position.
+ * @h: Pointer to the head of the doubly linked list.
+ * @idx: The index where the new node should be added.
+ * @n: The integer value for the new node.
+ *
+ * Return: Address of the new node, or NULL if it failed or index is out of bounds.
+ */
+
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 dlistint_t *new_node, *current;
