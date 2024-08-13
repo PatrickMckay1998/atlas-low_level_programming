1cc317c8ff59186f86fd0c996199941fabc3f180
patrick mckay <patrick.mckay@atlasschool.com>
2024-08-13 18:55:11 -0500

    Update for kernel, add file description

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -3,45 +3,53 @@
 #include <string.h>
 #include "lists.h"
 
+/**
+ * delete_dnodeint_at_index - Deletes a node at a given index.
+ * @head: Pointer to the head of the doubly linked list.
+ * @index: The index of the node to be deleted.
+ *
+ * Return: 1 if it succeeded, -1 if it failed.
+ */
+
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *current = *head;
-    dlistint_t *del;
-    unsigned int i = 0;
+dlistint_t *current = *head;
+dlistint_t *del;
+unsigned int i = 0;
 
-    if (*head == NULL)
-    {
-        return (-1);
-    }
+if (*head == NULL)
+{
+return (-1);
+}
 
-    while (current != NULL && i < index)
-    {
-        current = current->next;
-        i++;
-    }
+while (current != NULL && i < index)
+{
+current = current->next;
+i++;
+}
 
-    if (current == NULL)
-    {
-        return (-1);
-    }
+if (current == NULL)
+{
+return (-1);
+}
 
-    del = current;
+del = current;
 
-    if (del->prev != NULL)
-    {
-        del->prev->next = del->next;
-    }
+if (del->prev != NULL)
+{
+del->prev->next = del->next;
+}
 
-    if (del->next != NULL)
-    {
-        del->next->prev = del->prev;
-    }
+if (del->next != NULL)
+{
+del->next->prev = del->prev;
+}
 
-    if (del == *head)
-    {
-        *head = del->next;
-    }
-    free(del);
+if (del == *head)
+{
+*head = del->next;
+}
+free(del);
 
-    return (1);
+return (1);
 }
