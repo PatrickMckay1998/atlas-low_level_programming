a56cbe3f3133ecf79876d1fd5252924da3e94c23
patrick mckay <patrick.mckay@atlasschool.com>
2024-08-13 17:59:39 -0500

    UPdate file for betty

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -10,30 +10,30 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-    hash_node_t *node, *temp;
-    unsigned long int i;
+hash_node_t *node, *temp;
+unsigned long int i;
 
-    if (ht == NULL)
-        return;
+if (ht == NULL)
+return;
 
-    /* Iterate through each index of the hash table's array */
-    for (i = 0; i < ht->size; i++)
-    {
-        node = ht->array[i];
-        /* Free all nodes in the linked list at this index */
-        while (node != NULL)
-        {
-            temp = node->next;
-            free(node->key);
-            free(node->value);
-            free(node);
-            node = temp;
-        }
-    }
+/* Iterate through each index of the hash table's array */
+for (i = 0; i < ht->size; i++)
+{
+node = ht->array[i];
+/* Free all nodes in the linked list at this index */
+while (node != NULL)
+{
+temp = node->next;
+free(node->key);
+free(node->value);
+free(node);
+node = temp;
+}
+}
 
-    /* Free the array of pointers */
-    free(ht->array);
+/* Free the array of pointers */
+free(ht->array);
 
-    /* Free the hash table structure itself */
-    free(ht);
+/* Free the hash table structure itself */
+free(ht);
 }
