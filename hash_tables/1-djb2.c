dc35ad247ee88103bf79ea35d6a5210ce4d7e712
patrick mckay <patrick.mckay@atlasschool.com>
2024-08-13 16:27:23 -0500

    Add file description

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -1,5 +1,17 @@
 #include "hash_tables.h"
 
+/**
+ * hash_djb2 - Implements the djb2 hash function
+ * @str: The string to be hashed
+ *
+ * Description: This function implements the djb2 algorithm, a popular
+ *              and efficient hash function created by Dan Bernstein.
+ *              It processes each character of the input string and
+ *              generates a unique hash value.
+ *
+ * Return: The computed hash value for the given string
+ */
+
 unsigned long int hash_djb2(const unsigned char *str)
 {
 unsigned long int hash;
