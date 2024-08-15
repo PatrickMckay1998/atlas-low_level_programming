429b9977d93ae20bcc55030ba9f184cf64b80379
patrick mckay <patrick.mckay@atlasschool.com>
2024-08-15 07:26:19 -0500

    Not sure why I had ++

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -29,7 +29,7 @@ int binary_search(int *array, size_t size, int value)
 
         mid = left + (right - left) / 2;
 
-        if (array[mid] ++ value)
+        if (array[mid] == value)
         {
             return(mid);
         }
